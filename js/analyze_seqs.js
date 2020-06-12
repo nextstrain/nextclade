7ce022e003b0525ad6ebdb04d369437439aabb0c
Richard Neher <richard.neher@unibas.ch>
2020-06-12 14:58:20 +0200

    add comments

--- js/analyze_seqs.js
+++ js/analyze_seqs.js
@@ -43,33 +43,51 @@ function parseSequences(lines){
 
 
 function analyzeSeq(seq) {
-  const [query, ref] = alignPairwise(seq, rootSeq)
+  const {query, ref, score} = alignPairwise(seq, rootSeq)
+  console.log("Alignment score:", score);
+
   // report insertions
   let refPos = 0
+  let ins = ''
+  let insStart = -1
   ref.forEach((d,i) => {
-    if (d==='-'){
-      console.log(`insertion at position ${refPos+1}: ${query[i]}`)
+    if (d === "-") {
+      if (ins === "") {
+        insStart = refPos;
+      }
+      ins += query[i];
     } else {
-      refPos += 1
+      if (ins) {
+        console.log(`insertion at position ${insStart}: ${ins}`);
+        ins = "";
+      }
+      refPos += 1;
     }
   })
-
   // strip insertions relative to reference
   const refStripped = query.filter( (d, i) => ref[i]!=='-' )
 
   // report mutations
   let lastChar = -1
+  let nDel = 0
+  let delPos = -1
   let beforeAlignment = true
   refStripped.forEach((d,i) => {
     if (d!=='-') {
       if (beforeAlignment){
         console.log(`Alignment start: ${i+1}`);
         beforeAlignment = false;
+      } else if (nDel) {
+        console.log(`Deletion of length ${nDel} at ${delPos + 1}`);
+        nDel = 0
       }
       lastChar = i
     }
     if (d!=='-' && d!==rootSeq[i] && d!='N') {
       console.log(`Mutation at position ${i+1}: ${rootSeq[i]} --> ${d}`)
+    } else if (d==='-' && !beforeAlignment) {
+      if (!nDel) {delPos = i;}
+      nDel ++
     }
   })
   console.log(`Alignment end: ${lastChar+1}`);
