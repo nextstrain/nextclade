abbfbb5b0dd219616a3007a88c49461f3424c0a8
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-14 18:40:32 +0200

    refactor: extract common constants

--- packages/web/src/components/Layout/Footer.tsx
+++ packages/web/src/components/Layout/Footer.tsx
@@ -3,14 +3,22 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Col, Container, Row } from 'reactstrap'
 
+import { PROJECT_NAME, COMPANY_NAME } from 'src/constants'
+import { getCopyrightYearRange } from 'src/helpers/getCopyrightYearRange'
+
 export default function Footer() {
   const { t } = useTranslation()
+  const copyrightYearRange = getCopyrightYearRange()
 
   return (
     <Container fluid className="py-3">
       <Row noGutters>
         <Col xs={12} md={6} className="text-center text-md-left mb-2 mb-md-0">
-          {t('Web Clades (c) 2020 neherlab')}
+          {t('{{PROJECT_NAME}} (c) {{copyrightYearRange}} {{COMPANY_NAME}}', {
+            PROJECT_NAME,
+            copyrightYearRange,
+            COMPANY_NAME,
+          })}
         </Col>
       </Row>
     </Container>
