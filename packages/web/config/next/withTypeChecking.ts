d3a90d9e47e5e14052045bd25e54010cffff7e77
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-18 09:18:48 +0200

    chore: upgrade dependencies

--- packages/web/config/next/withTypeChecking.ts
+++ packages/web/config/next/withTypeChecking.ts
@@ -1,82 +1,16 @@
-import fs from 'fs-extra'
-import os from 'os'
-
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
-import chalk from 'chalk'
-import isInteractive from 'is-interactive'
-import { codeFrameColumns } from '@babel/code-frame'
 
-import { Issue, IssueSeverity, IssueOrigin } from 'fork-ts-checker-webpack-plugin/lib/issue'
-import { createInternalFormatter } from 'fork-ts-checker-webpack-plugin/lib/formatter'
 import { NextConfig } from '../../src/types/next'
 
 import { addWebpackPlugin } from './lib/addWebpackPlugin'
-
-function identity<T>(t: T) {
-  return t
-}
 
 export interface CreateFormatterParams {
   warningsAreErrors: boolean
 }
 
-export function createFormatter({ warningsAreErrors }: CreateFormatterParams) {
-  return (issue: Issue): string => {
-    const { origin, code, message, file, line, character } = issue
-
-    if (origin === IssueOrigin.INTERNAL) {
-      return createInternalFormatter()(issue)
-    }
-
-    const useColors = isInteractive()
-
-    // NOTE: this forces all issues to become warnings
-    issue.severity = IssueSeverity.WARNING
-    if (warningsAreErrors) {
-      // NOTE: this forces all issues to become errors
-      issue.severity = IssueSeverity.ERROR
-    }
-
-    const positionColor = chalk.dim
-
-    const source = file && fs.existsSync(file) && fs.readFileSync(file, 'utf-8')
-
-    let frame = ''
-    if (source && line) {
-      const location = {
-        start: {
-          line,
-          column: character,
-        },
-      }
-
-      frame = codeFrameColumns(source, location, {
-        highlightCode: useColors,
-      })
-        .split('\n')
-        .map((str) => `  ${str}`)
-        .join(os.EOL)
-    }
-
-    const codeColor = useColors ? chalk.grey : identity
-
-    const position = positionColor(`${line}:${character}`)
-
-    let errorCode = `${code}`
-    if (origin === 'typescript') {
-      errorCode = `TS${errorCode}`
-    }
-    errorCode = `(${errorCode})`
-    errorCode = codeColor(errorCode)
-
-    const eol = os.EOL
-
-    return `  ${file}:${position}${eol}  ${errorCode}${eol}  ${message}${eol}${eol}${frame}`
-  }
-}
-
 export interface GetWithTypeCheckingParams {
   eslint: boolean
+  typeChecking: boolean
   warningsAreErrors: boolean
   tsconfig: string
   reportFiles: string[]
@@ -85,6 +19,7 @@ export interface GetWithTypeCheckingParams {
 
 const getWithTypeChecking = ({
   eslint,
+  typeChecking,
   warningsAreErrors,
   tsconfig,
   reportFiles,
@@ -93,16 +28,27 @@ const getWithTypeChecking = ({
   return addWebpackPlugin(
     nextConfig,
     new ForkTsCheckerWebpackPlugin({
-      useTypescriptIncrementalApi: true,
-      measureCompilationTime: false,
-      memoryLimit,
-      eslint,
-      tsconfig,
-      formatter: createFormatter({ warningsAreErrors }),
-      checkSyntacticErrors: true,
-      async: true,
-      silent: false,
-      reportFiles,
+      typescript: {
+        memoryLimit,
+        mode: 'write-references',
+        diagnosticOptions: {
+          declaration: true,
+          global: true,
+          semantic: true,
+          syntactic: true,
+        },
+        profile: true,
+      },
+
+      eslint: eslint
+        ? {
+            enabled: true,
+            memoryLimit,
+            files: ['**/*.{js,jsx,ts,tsx}'],
+          }
+        : undefined,
+
+      formatter: 'codeframe',
     }),
   )
 }
