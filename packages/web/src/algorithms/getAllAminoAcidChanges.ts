978a9f6a6f05b63c9e383b46185a9634a27f4947
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-19 09:35:52 +0200

    fix: fix guard condition in aminoAcidChange()

--- packages/web/src/algorithms/getAllAminoAcidChanges.ts
+++ packages/web/src/algorithms/getAllAminoAcidChanges.ts
@@ -1,3 +1,5 @@
+import { inRange } from 'lodash'
+
 import { GeneMapDatum } from './geneMap'
 import { getCodon } from './codonTable'
 
@@ -5,7 +7,7 @@ import { Base } from './run'
 
 export function aminoAcidChange(pos: number, queryAllele: string, refSequence: string, gene: GeneMapDatum) {
   // check that the positions is infact part of this gene
-  if (pos < gene.start && pos >= gene.end) {
+  if (inRange(pos, gene.start, gene.end)) {
     return undefined
   }
 
@@ -14,7 +16,9 @@ export function aminoAcidChange(pos: number, queryAllele: string, refSequence: s
   const codon = (pos - gene.start + 1 - frame) / 3
   // pull out the codons and construct the query codon by inserting the allele
   const refCodon = refSequence.substring(pos - frame, pos - frame + 3)
-  const queryCodon = refCodon.substring(0, frame) + queryAllele + refCodon.substring(frame + 1, 3)
+  const queryBegin = refCodon.substring(0, frame)
+  const queryEnd = refCodon.substring(frame + 1, 3)
+  const queryCodon = queryBegin + queryAllele + queryEnd
 
   const refAA = getCodon(refCodon)
   const queryAA = getCodon(queryCodon)
