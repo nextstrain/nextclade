cae5fb1a94ea249fa060124a661f4e4ddaad37e2
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-24 12:27:41 +0200

    feat: implement reducer for updating results

--- packages/web/src/state/algorithm/algorithm.reducer.ts
+++ packages/web/src/state/algorithm/algorithm.reducer.ts
@@ -1,12 +1,83 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers'
 
-import { setParams } from './algorithm.actions'
-import { agorithmDefaultState } from './algorithm.state'
+import { algorithmRunAsync, analyzeAsync, parseAsync, setParams } from './algorithm.actions'
+import { agorithmDefaultState, AlgorithmStatus, AnylysisStatus } from './algorithm.state'
 
 import immerCase from '../util/fsaImmerReducer'
 
-export const agorithmReducer = reducerWithInitialState(agorithmDefaultState).withHandling(
-  immerCase(setParams, (draft, params) => {
-    draft.params = params
-  }),
-)
+export const agorithmReducer = reducerWithInitialState(agorithmDefaultState)
+  .withHandling(
+    immerCase(setParams, (draft, params) => {
+      draft.status = AlgorithmStatus.idling
+      draft.params = params
+    }),
+  )
+
+  .withHandling(
+    immerCase(algorithmRunAsync.started, (draft) => {
+      draft.status = AlgorithmStatus.started
+    }),
+  )
+
+  .withHandling(
+    immerCase(algorithmRunAsync.done, (draft) => {
+      draft.status = AlgorithmStatus.done
+    }),
+  )
+
+  .withHandling(
+    immerCase(algorithmRunAsync.failed, (draft) => {
+      draft.status = AlgorithmStatus.failed
+    }),
+  )
+
+  // parse
+  .withHandling(
+    immerCase(parseAsync.started, (draft) => {
+      draft.status = AlgorithmStatus.parsingStarted
+    }),
+  )
+
+  .withHandling(
+    immerCase(parseAsync.done, (draft, { result }) => {
+      draft.status = AlgorithmStatus.parsingDone
+      const resultState = result.map((seqName) => ({ status: AnylysisStatus.idling, seqName }))
+      draft.results = resultState
+    }),
+  )
+
+  .withHandling(
+    immerCase(parseAsync.failed, (draft, { error }) => {
+      draft.status = AlgorithmStatus.parsingFailed
+      draft.error = error
+    }),
+  )
+
+  // analyze
+  .withHandling(
+    immerCase(analyzeAsync.started, (draft, { seqName }) => {
+      draft.status = AlgorithmStatus.analysisStarted
+      draft.results = draft.results.map((result) =>
+        result.seqName === seqName ? { ...result, status: AnylysisStatus.started } : result,
+      )
+    }),
+  )
+
+  .withHandling(
+    immerCase(analyzeAsync.done, (draft, { params: { seqName }, result }) => {
+      draft.results = draft.results.map((oldResult) =>
+        oldResult.seqName === seqName
+          ? { ...oldResult, error: undefined, result, status: AnylysisStatus.done }
+          : oldResult,
+      )
+    }),
+  )
+
+  .withHandling(
+    immerCase(analyzeAsync.failed, (draft, { params: { seqName }, error }) => {
+      draft.status = AlgorithmStatus.failed
+      draft.results = draft.results.map((oldResult) =>
+        oldResult.seqName === seqName ? { ...oldResult, error, status: AnylysisStatus.failed } : oldResult,
+      )
+    }),
+  )
--- packages/web/src/state/algorithm/algorithm.state.ts
+++ packages/web/src/state/algorithm/algorithm.state.ts
@@ -2,17 +2,46 @@ import DEFAULT_INPUT from 'src/assets/data/defaultSequencesWithGaps.fasta'
 import DEFAULT_ROOT_SEQUENCE from 'src/assets/data/defaultRootSequence.txt'
 import { AnalysisResult } from 'src/algorithms/types'
 
+export enum AlgorithmStatus {
+  idling = 'idling',
+  started = 'started',
+  parsingStarted = 'parsingStarted',
+  parsingDone = 'parsingDone',
+  parsingFailed = 'parsingFailed',
+  analysisStarted = 'analysisStarted',
+  done = 'done',
+  failed = 'failed',
+}
+
+export enum AnylysisStatus {
+  idling = 'idling',
+  started = 'started',
+  done = 'done',
+  failed = 'failed',
+}
+
+export interface SequenceAnylysisState {
+  status: AnylysisStatus
+  seqName: string
+  result?: AnalysisResult
+}
+
 export interface AlgorithmState {
+  status: AlgorithmStatus
   params: {
     input: string
     rootSeq: string
   }
-  results?: AnalysisResult
+  results: SequenceAnylysisState[]
+  error?: Error
 }
 
 export const agorithmDefaultState: AlgorithmState = {
+  status: AlgorithmStatus.idling,
   params: {
     input: DEFAULT_INPUT,
     rootSeq: DEFAULT_ROOT_SEQUENCE,
   },
+  results: [],
+  error: undefined,
 }
