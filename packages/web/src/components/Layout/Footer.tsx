4ae2d37905dc7312d4fee98c405619e7290c71e9
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-24 16:54:27 +0200

    feat: add footer branding

--- packages/web/src/components/Layout/Footer.tsx
+++ packages/web/src/components/Layout/Footer.tsx
@@ -5,6 +5,12 @@ import { Col, Container, Row } from 'reactstrap'
 
 import { PROJECT_NAME, COMPANY_NAME } from 'src/constants'
 import { getCopyrightYearRange } from 'src/helpers/getCopyrightYearRange'
+import { LinkExternal } from 'src/components/Link/LinkExternal'
+
+import { ReactComponent as LogoNeherlab } from 'src/assets/img/neherlab.svg'
+import { ReactComponent as LogoBiozentrum } from 'src/assets/img/biozentrum.svg'
+import { ReactComponent as LogoUnibas } from 'src/assets/img/unibas.svg'
+import { ReactComponent as LogoVercel } from 'src/assets/img/powered-by-vercel.svg'
 
 export default function Footer() {
   const { t } = useTranslation()
@@ -13,12 +19,40 @@ export default function Footer() {
   return (
     <Container fluid className="py-3">
       <Row noGutters>
-        <Col xs={12} md={6} className="text-center text-md-left mb-2 mb-md-0">
-          {t('{{PROJECT_NAME}} (c) {{copyrightYearRange}} {{COMPANY_NAME}}', {
-            PROJECT_NAME,
-            copyrightYearRange,
-            COMPANY_NAME,
-          })}
+        <Col xs={12} md={4}>
+          <div>
+            {t('{{PROJECT_NAME}} (c) {{copyrightYearRange}} {{COMPANY_NAME}}', {
+              PROJECT_NAME,
+              copyrightYearRange,
+              COMPANY_NAME,
+            })}
+          </div>
+          <div>{t('Biozentrum')}</div>
+          <div>{t('University of Basel')}</div>
+        </Col>
+        <Col>
+          <Row>
+            <Col style={{ padding: '15px 10px', margin: '10px 10px' }}>
+              <LinkExternal url="">
+                <LogoNeherlab height={35} />
+              </LinkExternal>
+            </Col>
+            <Col style={{ backgroundColor: '#777', padding: '15px 10px', margin: '10px 10px' }}>
+              <LinkExternal url="">
+                <LogoBiozentrum height={35} />
+              </LinkExternal>
+            </Col>
+            <Col style={{ backgroundColor: '#777', padding: '15px 10px', margin: '10px 10px' }}>
+              <LinkExternal url="">
+                <LogoUnibas height={35} />
+              </LinkExternal>
+            </Col>
+            <Col style={{ padding: '15px 10px', margin: '10px 10px' }}>
+              <LinkExternal url="https://vercel.com/?utm_source=nextstrain">
+                <LogoVercel height={30} />
+              </LinkExternal>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
