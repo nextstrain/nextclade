92cdddcbcb362cf2e4dcfca51a7868c77f02f139
Richard Neher <richard.neher@unibas.ch>
2020-06-26 18:13:07 +0200

    show only last clade in table, increase divergence threshold

--- packages/web/src/algorithms/viruses.ts
+++ packages/web/src/algorithms/viruses.ts
@@ -7,7 +7,7 @@ export const VIRUSES: Record<string, Virus> = {
       knownClusters: new Set([28881, 28882, 28883]),
       windowSize: 100, // window along the genome to look for a cluster
       clusterCutOff: 6, // number of mutations within that window to trigger a cluster
-      divergenceThreshold: 15, // number of mutations to trigger divergence warning
+      divergenceThreshold: 20, // number of mutations to trigger divergence warning
       mixedSitesThreshold: 10, // number of non-ACGTN sites to trigger warning
       missingDataThreshold: 1000, // number of sites as N to trigger warning
     },
