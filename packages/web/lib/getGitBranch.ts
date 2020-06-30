5b56e885c5d24cbfe80a7fdea10808b6be25b91f
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-06-30 12:07:38 +0200

    chore: trigger vercel build

--- packages/web/lib/getGitBranch.ts
+++ packages/web/lib/getGitBranch.ts
@@ -3,6 +3,8 @@ import { execSync } from 'child_process'
 import { getenv } from './getenv'
 
 export function getGitBranch() {
+  console.log({ VERCEL_GITHUB_COMMIT_REF: process.env.VERCEL_GITHUB_COMMIT_REF })
+
   return (
     getenv('TRAVIS_BRANCH', null) ??
     getenv('NOW_GITHUB_COMMIT_REF', null) ??
