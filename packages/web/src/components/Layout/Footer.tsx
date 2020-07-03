f25e511e6976350fecd7830b8637da21ee89d3ae
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-07-03 18:20:36 +0200

    feat: add logos

--- packages/web/src/components/Layout/Footer.tsx
+++ packages/web/src/components/Layout/Footer.tsx
@@ -7,17 +7,32 @@ import styled from 'styled-components'
 import { PROJECT_NAME, COMPANY_NAME } from 'src/constants'
 import { getCopyrightYearRange } from 'src/helpers/getCopyrightYearRange'
 import { LinkExternal } from 'src/components/Link/LinkExternal'
-
-import { ReactComponent as LogoVercel } from 'src/assets/img/powered-by-vercel.svg'
 import { getVersionString } from 'src/helpers/getVersionString'
+
+import { ReactComponent as LogoBedfordlab } from 'src/assets/img/bedfordlab.svg'
+import { ReactComponent as LogoBiozentrum } from 'src/assets/img/biozentrum_square.svg'
+import { ReactComponent as LogoFredHutch } from 'src/assets/img/fred_hutch.svg'
+import { ReactComponent as LogoNeherlab } from 'src/assets/img/neherlab.svg'
+// import { ReactComponent as LogoNextstrain } from 'src/assets/img/nextstrain_logo.svg'
+// import { ReactComponent as LogoUnibas } from 'src/assets/img/unibas.svg'
+import { ReactComponent as LogoVercel } from 'src/assets/img/powered-by-vercel.svg'
 
 const FooterContainer = styled(Container)`
   background-color: #2a2a2a;
   color: #c4cdd5;
+  padding: 5px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
 `
+
+const LogoContainer = styled.div``
 
 const VersionText = styled.div`
   font-size: 0.75rem;
+`
+
+const LogoLink = styled(LinkExternal)`
+  padding: 10px 20px;
 `
 
 const CopyrightText = styled.div`
@@ -40,11 +55,27 @@ export default function Footer() {
             })}
           </CopyrightText>
 
-          <div className="mx-auto mt-2 mb-1">
-            <LinkExternal className="my-auto" url="https://vercel.com/?utm_source=nextstrain">
+          <LogoContainer className="mx-auto">
+            <LogoLink url="https://neherlab.org">
+              <LogoNeherlab height={35} />
+            </LogoLink>
+
+            <LogoLink url="https://www.biozentrum.unibas.ch">
+              <LogoBiozentrum height={27} />
+            </LogoLink>
+
+            <LogoLink url="https://bedford.io">
+              <LogoBedfordlab height={27} />
+            </LogoLink>
+
+            <LogoLink url="https://www.fredhutch.org">
+              <LogoFredHutch height={27} />
+            </LogoLink>
+
+            <LogoLink className="my-auto" url="https://vercel.com/?utm_source=nextstrain">
               <LogoVercel height={25} />
-            </LinkExternal>
-          </div>
+            </LogoLink>
+          </LogoContainer>
 
           <VersionText className="ml-auto my-auto">{getVersionString()}</VersionText>
         </Col>
