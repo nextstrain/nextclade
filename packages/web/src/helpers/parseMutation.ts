8e88955b6a83d7e58c9b22d500e9be8d19ce715d
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-07-09 02:06:15 +0200

    feat: add mutation parser

---
+++ packages/web/src/helpers/parseMutation.ts
@@ -0,0 +1,50 @@
+/* eslint-disable security/detect-unsafe-regex */
+import { Optional } from 'utility-types'
+import type { Nucleotide, NucleotideSubstitution } from 'src/algorithms/types'
+
+export const NUCLEOTIDE_WILDCARD = '.' as const
+
+export function parseNucleotideCharacter(raw: string | undefined | null) {
+  if (!raw || raw.length === 0 || raw === NUCLEOTIDE_WILDCARD) {
+    return undefined
+  }
+  return raw.toUpperCase() as Nucleotide
+}
+
+export function parseNucleotidePosition(raw: string | undefined | null) {
+  if (!raw || raw.length === 0 || raw === NUCLEOTIDE_WILDCARD) {
+    return undefined
+  }
+
+  const num = Number.parseInt(raw ?? '', 10)
+
+  if (!Number.isFinite(num)) {
+    return undefined
+  }
+
+  return num - 1
+}
+
+export function parseMutation(formatted: string): Optional<NucleotideSubstitution> | undefined {
+  const match = /^(?<refNuc>[.a-z-])(?<pos>(\d)*)(?<queryNuc>[.a-z-]{0,1})$/i.exec(formatted)
+
+  if (!match?.groups) {
+    return undefined
+  }
+
+  if (Object.values(match?.groups).every((s) => s.length === 0)) {
+    return undefined
+  }
+
+  const refNuc = parseNucleotideCharacter(match.groups?.refNuc)
+  const pos = parseNucleotidePosition(match.groups?.pos)
+  const queryNuc = parseNucleotideCharacter(match.groups?.queryNuc)
+
+  const result = { refNuc, pos, queryNuc }
+
+  if (Object.values(result).every((r) => r === undefined)) {
+    return undefined
+  }
+
+  return result
+}
