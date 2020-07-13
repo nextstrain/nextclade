5bcb010a0b0cc77a70fced1da1314dc2ff7503fa
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-07-13 11:04:03 +0200

    chore(ci): add packtracker

---
+++ packages/web/config/next/withPacktracker.ts
@@ -0,0 +1,22 @@
+import PacktrackerPlugin from '@packtracker/webpack-plugin'
+import type { NextConfig } from 'next'
+import { addWebpackPlugin } from './lib/addWebpackPlugin'
+import { getenv } from '../../lib/getenv'
+
+const withPacktracker = () => (nextConfig: NextConfig) => {
+  const PT_PROJECT_TOKEN = getenv('PT_PROJECT_TOKEN', null)
+  if (!PT_PROJECT_TOKEN) {
+    return nextConfig
+  }
+
+  return addWebpackPlugin(
+    nextConfig,
+    new PacktrackerPlugin({
+      project_token: PT_PROJECT_TOKEN,
+      upload: true,
+      fail_build: true,
+    }),
+  )
+}
+
+export default withPacktracker
