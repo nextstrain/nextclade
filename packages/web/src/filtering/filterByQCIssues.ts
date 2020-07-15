27675e0f23dbe3354edf98bb3cf9742b8af1b9ba
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-07-15 09:25:56 +0200

    feat adjust filtering to the new QC results

--- packages/web/src/filtering/filterByQCIssues.ts
+++ packages/web/src/filtering/filterByQCIssues.ts
@@ -3,8 +3,8 @@ import type { SequenceAnylysisState } from 'src/state/algorithm/algorithm.state'
 export function filterByQCIssues(hasNoQcIssuesFilter: boolean, hasQcIssuesFilter: boolean, hasErrorsFilter: boolean) {
   return ({ result, errors }: SequenceAnylysisState) => {
     const hasErrors = errors.length > 0
-    const hasIssues = hasErrors || (result && result.diagnostics.flags.length > 0)
-    const hasNoIssues = result && result.diagnostics.flags.length === 0
+    const hasIssues = hasErrors || (result && result.diagnostics.score > 0)
+    const hasNoIssues = result && result.diagnostics.score === 0
     return (hasNoQcIssuesFilter && hasNoIssues) || (hasQcIssuesFilter && hasIssues) || (hasErrorsFilter && hasErrors)
   }
 }
