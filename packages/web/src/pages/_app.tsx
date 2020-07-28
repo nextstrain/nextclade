db21817bbd8da31f1eac014f2d96e8f945524a1b
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-07-28 16:36:36 +0200

    feat: use nexstrain logo spinner for loading page

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -8,7 +8,7 @@ import 'src/helpers/errorPrototypeTojson' // to visualize Error in Redux Dev Too
 
 import { enableES5 } from 'immer'
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
 
 import { AppProps } from 'next/app'
 import type { Store } from 'redux'
@@ -24,6 +24,7 @@ import { MDXProvider } from '@mdx-js/react'
 import { initialize } from 'src/initialize'
 import i18n from 'src/i18n/i18n'
 
+import Loading from 'src/components/Loading/Loading'
 import { LinkExternal } from 'src/components/Link/LinkExternal'
 import { SEO } from 'src/components/Common/SEO'
 
@@ -50,25 +51,27 @@ export default function MyApp({ Component, pageProps, router }: AppProps) {
   }, [router])
 
   if (!state) {
-    return 'Loading'
+    return <Loading />
   }
 
   const { store, persistor } = state
 
   return (
-    <Provider store={store}>
-      <ConnectedRouter>
-        <ThemeProvider theme={theme}>
-          <MDXProvider components={{ a: LinkExternal }}>
-            <PersistGate loading={null} persistor={persistor}>
-              <I18nextProvider i18n={i18n}>
-                <SEO />
-                <Component {...pageProps} />
-              </I18nextProvider>
-            </PersistGate>
-          </MDXProvider>
-        </ThemeProvider>
-      </ConnectedRouter>
-    </Provider>
+    <Suspense fallback={<Loading />}>
+      <Provider store={store}>
+        <ConnectedRouter>
+          <ThemeProvider theme={theme}>
+            <MDXProvider components={{ a: LinkExternal }}>
+              <PersistGate loading={null} persistor={persistor}>
+                <I18nextProvider i18n={i18n}>
+                  <SEO />
+                  <Component {...pageProps} />
+                </I18nextProvider>
+              </PersistGate>
+            </MDXProvider>
+          </ThemeProvider>
+        </ConnectedRouter>
+      </Provider>
+    </Suspense>
   )
 }
