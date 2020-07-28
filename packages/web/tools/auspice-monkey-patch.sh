8a501440b10d7d31cdc06f44a19547c487b568fd
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-07-28 10:38:33 +0200

    fix: patch auspice to remove more references to window

--- packages/web/tools/auspice-monkey-patch.sh
+++ packages/web/tools/auspice-monkey-patch.sh
@@ -12,3 +12,13 @@ sed -i.bak '/import "\.\.\/\.\.\/css\/awesomplete\.css";/d' node_modules/auspice
 # Removes requires for '@extensions' modules from `extensions.js`
 # Reason: We don't use extensions and don't want to setup webpack aliases for that.
 sed -i.bak '/.*@extensions.*/d' node_modules/auspice/src/util/extensions.js
+
+# Removes references to `window`
+# Reason: Next.js prerendering step is performed server-side and does not have access to `window` global
+sed -i.bak  's/sidebarOpen: window.innerWidth > controlsHiddenWidth,/sidebarOpen: true,/g' node_modules/auspice/src/reducers/controls.js
+sed -i.bak  's/if (window.location.pathname.includes("gisaid")) {/if (false) {/g' node_modules/auspice/src/reducers/controls.js
+sed -i.bak  's/width: window.innerWidth,/width: 0,/g' node_modules/auspice/src/reducers/browserDimensions.js
+sed -i.bak  's/height: window.innerHeight,/height: 0,/g' node_modules/auspice/src/reducers/browserDimensions.js
+sed -i.bak  's/docHeight: window.document.body.clientHeight/docHeight: 0,/g' node_modules/auspice/src/reducers/browserDimensions.js
+
+# sed -i.bak  's///g' node_modules/auspice/src/reducers/browserDimensions.js
