4129947ad55e5319c63d5058228eb873f7ddd30b
Richard Neher <richard.neher@unibas.ch>
2020-08-29 19:07:25 +0200

    fix: adjust QC filter to new scoring scheme

--- packages/web/src/filtering/filterByQCIssues.ts
+++ packages/web/src/filtering/filterByQCIssues.ts
@@ -10,11 +10,11 @@ export function filterByQCIssues({ hasNoQcIssuesFilter, hasQcIssuesFilter, hasEr
   return ({ result, qc, errors }: SequenceAnalysisState) => {
     const hasErrors = errors.length > 0
 
-    const hasIssues = qc && qc.score > 0
+    const hasIssues = qc && qc.score > 100
 
     // The sequences which are still being processed (!result || !qc) are presumed to have no issues
     // until QC results come and prove otherwise
-    const hasNoIssues = (!hasErrors && (!result || !qc)) || (qc && qc.score === 0)
+    const hasNoIssues = (!hasErrors && (!result || !qc)) || (qc && qc.score < 100)
 
     return (hasNoQcIssuesFilter && hasNoIssues) || (hasQcIssuesFilter && hasIssues) || (hasErrorsFilter && hasErrors)
   }
