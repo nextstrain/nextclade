bc6d0fc6e1af496b7b38bc6cae90663a105236e8
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-09-05 00:12:36 +0200

    chore(dev): ensure sanitizeError returns correct type

--- packages/web/src/helpers/sanitizeError.ts
+++ packages/web/src/helpers/sanitizeError.ts
@@ -1,9 +1,9 @@
 import serializeJavascript from 'serialize-javascript'
 
-export function sanitizeError(error: Error) {
+export function sanitizeError(error: unknown): Error {
   if (!(error instanceof Error)) {
     const str = serializeJavascript(error, { space: 2, ignoreFunction: true })
-    return new TypeError(`developer error: error object is not recognized:
+    return new Error(`developer error: error object is not recognized:
       ${str}
       Make sure all exceptions are derived from \`Error\``)
   }
