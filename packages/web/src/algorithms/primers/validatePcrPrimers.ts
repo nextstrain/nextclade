643607b2a435a93267479251dbb5da086bbb48fa
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-09-25 13:43:23 +0200

    feat(cli): add possibility to override clades, primers and gene map

--- packages/web/src/algorithms/primers/validatePcrPrimers.ts
+++ packages/web/src/algorithms/primers/validatePcrPrimers.ts
@@ -1,6 +1,12 @@
+import type { PrimerEntries } from 'src/algorithms/primers/convertPcrPrimers'
 import type { PcrPrimer } from 'src/algorithms/types'
 
+export function validatePcrPrimerEntries(primersEntriesUnsafe: unknown) {
+  // TODO: validate PCR primers CSV entries
+  return primersEntriesUnsafe as PrimerEntries[]
+}
+
 export function validatePcrPrimers(primersUnsafe: unknown) {
-  // TODO
+  // TODO: validate PCR primers JSON
   return primersUnsafe as PcrPrimer[]
 }
