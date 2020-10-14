5273f6345de4ccc2d9ac6e36d3ce95e8310f3184
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-10-14 12:20:13 +0200

    chore: fix webpack types

--- packages/web/config/next/lib/addWebpackPlugin.ts
+++ packages/web/config/next/lib/addWebpackPlugin.ts
@@ -1,12 +1,12 @@
 import type { NextWebpackOptions, NextConfig } from 'next'
-import type { WebpackOptions, WebpackPluginFunction, WebpackPluginInstance } from 'webpack/declarations/WebpackOptions'
+import type { Configuration, WebpackPluginFunction, WebpackPluginInstance } from 'webpack'
 
 import { addWebpackConfig } from './addWebpackConfig'
 
 export function addWebpackPlugin(nextConfig: NextConfig, plugin: WebpackPluginInstance | WebpackPluginFunction) {
   return addWebpackConfig(
     nextConfig,
-    (nextConfig: NextConfig, webpackConfig: WebpackOptions, { isServer }: NextWebpackOptions) => {
+    (nextConfig: NextConfig, webpackConfig: Configuration, { isServer }: NextWebpackOptions) => {
       if (!isServer) {
         if (webpackConfig?.plugins) {
           webpackConfig.plugins.push(plugin)
