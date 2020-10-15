719d0b00cd4ffb0c0ed820ef6868e7ccef62d909
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-10-15 09:57:56 +0200

    chore: setup clang-tidy

--- packages/algorithms/build.sh
+++ packages/algorithms/build.sh
@@ -18,14 +18,48 @@ rm -rf "${BUILD_DIR}/*"
 
 pushd "${BUILD_DIR}" >/dev/null
 
-emcmake cmake .. \
+emcmake cmake "${THIS_DIR}" \
   -DCMAKE_BUILD_TYPE=Debug \
-  -DCMAKE_VERBOSE_MAKEFILE=1
+  -DCMAKE_VERBOSE_MAKEFILE=1 \
+  -DCMAKE_COLOR_MAKEFILE=1 \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 
 emmake make -j 8
 
-cp *.js *.map *.wasm "${RESULT_DIR}"
-
 popd >/dev/null
+
+run-clang-tidy -quiet -p "${BUILD_DIR}" \
+  -extra-arg="--target=wasm32-unknown-emscripten" \
+  -extra-arg="-D__EMSCRIPTEN__" \
+  -extra-arg="-D__EMSCRIPTEN_major__=2" \
+  -extra-arg="-D__EMSCRIPTEN_minor__=0" \
+  -extra-arg="-D__EMSCRIPTEN_tiny__=6" \
+  -extra-arg="-D_LIBCPP_ABI_VERSION=2" \
+  -extra-arg="-Dunix" \
+  -extra-arg="-D__unix" \
+  -extra-arg="-D__unix__" \
+  -extra-arg="-m32" \
+  -extra-arg="-msse" \
+  -extra-arg="-msse2" \
+  -extra-arg="-msse3" \
+  -extra-arg="-mssse3" \
+  -extra-arg="-msse4.1" \
+  -extra-arg="-msse4.2" \
+  -extra-arg="-mavx" \
+  -extra-arg="-mfpu=neon" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/lib/libc/musl/arch/emscripten" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/lib/libc/musl/arch/js" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/local/include" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/include/compat" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/include" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/include/libcxx" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/lib/compiler-rt/include" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/lib/libcxxabi/include" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/include/libc" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/include/gfx" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/system/include/SDL" \
+  -extra-arg="-isystem${EMSDK}/upstream/emscripten/lib/clang/12.0.0/include"
+
+cp ${BUILD_DIR}/*.{js,map,wasm} "${RESULT_DIR}"
 
 echo "done"
--- packages/algorithms/src/add.cpp
+++ packages/algorithms/src/add.cpp
@@ -1,8 +1,6 @@
-#include <emscripten.h>
 #include <emscripten/bind.h>
 
 #include <exception>
-#include <iostream>
 #include <string>
 
 
@@ -14,15 +12,18 @@ std::string concat(const std::string& x, const std::string& y) {
   return x + y;
 }
 
+
 void kaboom() {
   throw std::runtime_error("Error: in " + std::string(__FILE__) + ":" + std::to_string(__LINE__) + ": kaboom!");
 }
 
-std::string getExceptionMessage(std::intptr_t exceptionPtr) {
+std::string getExceptionMessage(std::intptr_t exceptionPtr) {// NOLINT(misc-unused-parameters)
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-init-variables)
   const std::exception* e = reinterpret_cast<std::runtime_error*>(exceptionPtr);
   return e->what();
 }
 
+// NOLINTNEXTLINE(cert-err58-cpp,cppcoreguidelines-avoid-non-const-global-variables)
 EMSCRIPTEN_BINDINGS(add) {
   emscripten::function("add", &add);
   emscripten::function("concat", &concat);
