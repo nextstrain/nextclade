585371a1e397bc15e7802d8e6a4e2edaa5777da9
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-11-04 21:29:39 +0100

    feat: fetch input root sequence and input ref tree provided  URL params

--- packages/web/src/io/fetchInputsAndRunMaybe.ts
+++ packages/web/src/io/fetchInputsAndRunMaybe.ts
@@ -1,10 +1,12 @@
 import type { Router } from 'next/router'
 import type { Dispatch } from 'redux'
 import Axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import { treeValidate } from 'src/algorithms/tree/treeValidate'
 
 import { takeFirstMaybe } from 'src/helpers/takeFirstMaybe'
-import { algorithmRunAsync, setIsDirty } from 'src/state/algorithm/algorithm.actions'
+import { algorithmRunAsync, setIsDirty, setRootSeq, setTree } from 'src/state/algorithm/algorithm.actions'
 import { errorAdd } from 'src/state/error/error.actions'
+import { sanitizeRootSeq } from 'src/helpers/sanitizeRootSeq'
 
 export class HttpRequestError extends Error {
   public readonly request: AxiosRequestConfig
@@ -17,20 +19,51 @@ export class HttpRequestError extends Error {
   }
 }
 
+export interface FetchParams {
+  url?: string
+  dispatch: Dispatch
+}
+
+export async function fetchMaybe<T>(url?: string) {
+  if (url) {
+    const { data } = await Axios.get<T | undefined>(url)
+    return data
+  }
+  return undefined
+}
+
 export async function fetchInputsAndRunMaybe(dispatch: Dispatch, router: Router) {
   const inputFastaUrl = takeFirstMaybe(router.query?.['input-fasta'])
-  if (inputFastaUrl) {
-    try {
-      const { data } = await Axios.get<string | undefined>(inputFastaUrl)
-      if (data) {
-        dispatch(setIsDirty(true))
-        dispatch(algorithmRunAsync.trigger(data))
-        await router.replace('/results')
-      }
-    } catch (error_) {
-      const error = new HttpRequestError(error_ as AxiosError)
-      console.error(error)
-      dispatch(errorAdd({ error }))
-    }
+  const inputRootSeqUrl = takeFirstMaybe(router.query?.['input-root-seq'])
+  const inputTreeUrl = takeFirstMaybe(router.query?.['input-tree'])
+
+  let inputFasta: string | undefined
+  let inputRootSeqDangerous: string | undefined
+  let inputTreeDangerous: string | undefined
+
+  try {
+    inputFasta = await fetchMaybe(inputFastaUrl)
+    inputRootSeqDangerous = await fetchMaybe(inputRootSeqUrl)
+    inputTreeDangerous = await fetchMaybe(inputTreeUrl)
+  } catch (error_) {
+    const error = new HttpRequestError(error_ as AxiosError)
+    console.error(error)
+    dispatch(errorAdd({ error }))
+  }
+
+  if (inputRootSeqDangerous) {
+    const inputRootSeq = sanitizeRootSeq(inputRootSeqDangerous)
+    dispatch(setRootSeq(inputRootSeq))
+  }
+
+  if (inputTreeDangerous) {
+    const inputTree = treeValidate(inputTreeDangerous)
+    dispatch(setTree(inputTree))
+  }
+
+  if (inputFasta) {
+    dispatch(setIsDirty(true))
+    dispatch(algorithmRunAsync.trigger(inputFasta))
+    await router.replace('/results')
   }
 }
