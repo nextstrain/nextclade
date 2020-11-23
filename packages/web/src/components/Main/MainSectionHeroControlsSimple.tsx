9344d923233956c37ea2ac1dfd4432dd27bf2149
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-11-23 09:28:04 +0100

    feat: use new file picker in simple mode too

--- packages/web/src/components/Main/MainSectionHeroControlsSimple.tsx
+++ packages/web/src/components/Main/MainSectionHeroControlsSimple.tsx
@@ -5,11 +5,13 @@ import { connect } from 'react-redux'
 import { push } from 'connected-next-router'
 import { useTranslation } from 'react-i18next'
 import { Button, Card, CardBody, CardHeader, Col, Input, Row } from 'reactstrap'
+import styled from 'styled-components'
 import { FaCaretRight } from 'react-icons/fa'
 import { MdClear, MdPlayArrow } from 'react-icons/md'
 
 import { getSequenceDatum } from 'src/algorithms/defaults/viruses'
 import { AlgorithmParams } from 'src/algorithms/types'
+import { FilePicker } from 'src/components/Main/FilePicker'
 import { MainSectionHeroFeatures } from 'src/components/Main/MainSectionHeroFeatures'
 import { Uploader } from 'src/components/Main/Uploader'
 import {
@@ -24,6 +26,11 @@ import type { FileStats } from 'src/state/algorithm/algorithm.state'
 
 import type { State } from 'src/state/reducer'
 import { setShowInputBox } from 'src/state/ui/ui.actions'
+import { FileIconFasta } from './UploaderFileIcons'
+
+export const FilePickerSimple = styled(FilePicker)`
+  height: 100%;
+`
 
 export interface MainSectionHeroControlsProps {
   params: AlgorithmParams
@@ -111,20 +118,6 @@ export function MainSectionHeroControlsDisconnected({
     algorithmRunTrigger(file)
   }
 
-  const runButton = (
-    <Button className="mx-auto btn-refresh" color="success" onClick={handleRunButtonClick}>
-      <MdPlayArrow className="btn-icon" />
-      <span>{t('Run')}</span>
-    </Button>
-  )
-
-  const toResultsButton = (
-    <Button className="mx-auto btn-refresh" color="primary" onClick={goToResults}>
-      <span className="mr-2">{t('To Results')}</span>
-      <FaCaretRight />
-    </Button>
-  )
-
   return (
     <Row noGutters className="hero-content">
       <Col xl={6} className="px-lg-4 hero-content-left">
@@ -135,62 +128,14 @@ export function MainSectionHeroControlsDisconnected({
         <div className="hero-content-left-card">
           <Row>
             <Col>
-              <Uploader onUpload={onUpload} />
+              <FilePickerSimple
+                canCollapse={false}
+                defaultCollapsed={false}
+                icon={<FileIconFasta />}
+                text={t('Sequences')}
+                onUpload={onUpload}
+              />
             </Col>
-          </Row>
-
-          <Row className="my-2" hidden={showInputBox}>
-            <Col className="d-flex">
-              <p className="mx-auto">{t('OR')}</p>
-            </Col>
-          </Row>
-
-          <Row className="mb-2" hidden={showInputBox}>
-            <Col className="d-flex">
-              <Button className="mx-auto btn-select-file" onClick={() => setShowInputBox(true)}>
-                {t('Paste sequences')}
-              </Button>
-            </Col>
-          </Row>
-
-          <Row className="mb-2" hidden={!showInputBox}>
-            <Col>
-              <Card>
-                <CardHeader className="d-flex">
-                  <div className="mr-auto">{t('Paste or edit sequences')}</div>
-                  <div className="ml-auto">
-                    <button type="button" className="button-transparent" onClick={() => setShowInputBox(false)}>
-                      <MdClear />
-                    </button>
-                  </div>
-                </CardHeader>
-
-                <CardBody className="p-0">
-                  <Input
-                    className="sequence-input"
-                    type="textarea"
-                    data-gramm_editor="false"
-                    autoComplete="off"
-                    autoCorrect="off"
-                    autoCapitalize="off"
-                    spellCheck="false"
-                    data-gramm="false"
-                    wrap="off"
-                    name="sequence-input"
-                    id="sequence-input"
-                    cols={80}
-                    rows={20}
-                    value={params.sequenceDatum}
-                    onChange={handleInputChange}
-                    innerRef={inputRef}
-                  />
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-
-          <Row className="mb-2" hidden={!showInputBox}>
-            <Col className="d-flex w-100">{isDirty ? runButton : toResultsButton}</Col>
           </Row>
 
           <Row>
