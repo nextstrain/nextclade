6a333a98eac683c57c4ddae7f8bbc23cec300faa
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-11-24 13:07:38 +0100

    chore: fix reducer hot reloading

--- packages/web/src/state/store.ts
+++ packages/web/src/state/store.ts
@@ -65,7 +65,7 @@ export async function configureStore({ router, workerPools }: ConfigureStorePara
 
   if (module.hot) {
     // Setup hot reloading of root reducer
-    module.hot.accept(() => {
+    module.hot.accept('./reducer', () => {
       store.replaceReducer(createRootReducer())
       console.info('[HMR] root reducer reloaded successfully')
     })
