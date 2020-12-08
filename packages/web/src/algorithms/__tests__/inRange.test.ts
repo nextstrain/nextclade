427cb3c2bf73366696390434faee1d09c1414b34
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-12-08 03:39:45 +0100

    test: add range boundary cases to inRange() test

--- packages/web/src/algorithms/__tests__/inRange.test.ts
+++ packages/web/src/algorithms/__tests__/inRange.test.ts
@@ -15,6 +15,10 @@ describe('inRange', () => {
     expect(inRange(0, { begin: -7, end: 21 })).toBeTrue()
   })
 
+  it('should accept lower boundary', () => {
+    expect(inRange(-7, { begin: -7, end: 21 })).toBeTrue()
+  })
+
   // Rejects
 
   it('should reject not contained left', () => {
@@ -27,5 +31,9 @@ describe('inRange', () => {
 
   it('should reject when range is empty', () => {
     expect(inRange(7, { begin: 7, end: 7 })).toBeFalse()
+  })
+
+  it('should reject upper boundary', () => {
+    expect(inRange(21, { begin: -7, end: 21 })).toBeFalse()
   })
 })
