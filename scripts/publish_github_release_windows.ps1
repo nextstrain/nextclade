adfb28d71d73106fd67e46000ac5d461ff2bccec
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-02-11 10:14:03 +0100

    chore: fix ghr path

--- scripts/publish_github_release_windows.ps1
+++ scripts/publish_github_release_windows.ps1
@@ -25,7 +25,11 @@ Start-BitsTransfer `
 
 Expand-Archive "ghr_v${GHR_VERSION}_windows_amd64.zip"
 
-Move-Item -Path "ghr_v${GHR_VERSION}_windows_amd64/ghr.exe" -Destination . -Force
+Get-ChildItem .
+
+Get-ChildItem "ghr_v${GHR_VERSION}_windows_amd64"
+
+Move-Item -Path "ghr_v${GHR_VERSION}_windows_amd64\ghr.exe" -Destination . -Force
 
 Get-ChildItem $INSTALL_DIR/bin
 
