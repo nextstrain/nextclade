431ff633512eee69831856918160b2994228bf76
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-03-16 07:25:01 +0100

    fix: avoid nullptr dereference

--- packages/nextclade/src/tree/TreeNodeArray.cpp
+++ packages/nextclade/src/tree/TreeNodeArray.cpp
@@ -12,7 +12,7 @@ namespace Nextclade {
   TreeNodeArray::TreeNodeArray(rapidjson::Value* value) : value(value) {}
 
   TreeNodeArray TreeNodeArray::filter(const std::function<bool(TreeNode)>& predicate) const {
-    if (value->IsArray()) {
+    if (value && value->IsArray()) {
       auto a = rapidjson::MemoryPoolAllocator();
       rapidjson::Value result;
       result.SetArray();
@@ -28,7 +28,7 @@ namespace Nextclade {
   }
 
   void TreeNodeArray::forEach(const std::function<void(TreeNode)>& action) const {
-    if (value->IsArray()) {
+    if (value && value->IsArray()) {
       auto a = rapidjson::MemoryPoolAllocator();
       rapidjson::Value result;
       for (auto&& item : value->GetArray()) {
