df5476b1921eaa2a597ed6f17fed2c93784f22f6
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-04-14 14:37:37 +0200

    fix: correctly use private mutations, not all mutations, in the QC rule

--- packages/nextclade/src/qc/rulePrivateMutations.cpp
+++ packages/nextclade/src/qc/rulePrivateMutations.cpp
@@ -9,19 +9,19 @@
 #include "getQcRuleStatus.h"
 
 namespace Nextclade {
-  std::optional<QcResultPrivateMutations> rulePrivateMutations(//
-    const NextcladeResult& result,                             //
-    const std::vector<NucleotideSubstitution>&,                //
-    const QCRulesConfigPrivateMutations& config                //
+  std::optional<QcResultPrivateMutations> rulePrivateMutations( //
+    const NextcladeResult& result,                              //
+    const std::vector<NucleotideSubstitution>& privateMutations,//
+    const QCRulesConfigPrivateMutations& config                 //
   ) {
     if (!config.enabled) {
       return {};
     }
 
-    const auto& substitutions = result.substitutions;
     const auto& insertions = result.insertions;
     const auto& deletions = result.deletions;
-    const auto totalNumberOfMutations = safe_cast<double>(substitutions.size() + insertions.size() + deletions.size());
+    const auto totalNumberOfMutations =
+      safe_cast<double>(privateMutations.size() + insertions.size() + deletions.size());
 
     // the score hits 100 if the excess mutations equals the cutoff value
     const auto score = (std::max(0.0, totalNumberOfMutations - config.typical) * 100.0) / config.cutoff;
