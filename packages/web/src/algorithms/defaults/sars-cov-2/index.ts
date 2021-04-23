24cee0aef5f7cccfdca9215a88912db3583e4542
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-04-23 13:26:10 +0200

    chore: revert changes to the tree import in nextclade.js

--- packages/web/src/algorithms/defaults/sars-cov-2/index.ts
+++ packages/web/src/algorithms/defaults/sars-cov-2/index.ts
@@ -2,14 +2,13 @@ import type { VirusRaw } from 'src/algorithms/types'
 import { VirusName } from 'src/algorithms/defaults/virusNames'
 import { qcRulesConfig } from 'src/algorithms/defaults/sars-cov-2/qcRulesConfig'
 import { rootSeq } from 'src/algorithms/defaults/sars-cov-2/rootSeq'
+import auspiceDataRaw from 'src/algorithms/defaults/sars-cov-2/tree.json'
 import sequenceData from 'src/algorithms/defaults/sars-cov-2/sequenceData.fasta'
 import pcrPrimers from 'src/algorithms/defaults/sars-cov-2/pcrPrimers.json'
 
 import { treeValidate } from 'src/algorithms/tree/treeValidate'
 import { validatePcrPrimers } from 'src/algorithms/primers/validatePcrPrimers'
 import { convertGeneMap, getGeneMapJsonFromTree } from 'src/io/convertGeneMap'
-
-import auspiceDataRaw from '../../../../../../data/sars-cov-2/tree.json'
 
 const auspiceData = treeValidate(auspiceDataRaw)
 const geneMap = convertGeneMap(getGeneMapJsonFromTree(auspiceData))
