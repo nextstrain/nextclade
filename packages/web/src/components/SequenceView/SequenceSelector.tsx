cbce82317067e2f09d64716b9a961c1fc7ccf8c7
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-06-03 03:29:13 +0200

    feat: improve gene selector text

--- packages/web/src/components/SequenceView/SequenceSelector.tsx
+++ packages/web/src/components/SequenceView/SequenceSelector.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 
 import { connect } from 'react-redux'
 
+import { useTranslationSafe } from 'src/helpers/useTranslationSafe'
+
 import type { Gene } from 'src/algorithms/types'
 import type { State } from 'src/state/reducer'
-
 import { selectGeneMap } from 'src/state/algorithm/algorithm.selectors'
 
 export interface SequenceSelectorProps {
@@ -23,6 +24,8 @@ const mapDispatchToProps = {}
 export const SequenceSelector = connect(mapStateToProps, mapDispatchToProps)(SequenceSelectorDisconnected)
 
 export function SequenceSelectorDisconnected({ geneMap, viewedGene, setViewedGene }: SequenceSelectorProps) {
+  const { t } = useTranslationSafe()
+
   let geneNames = ['Sequence']
   if (geneMap) {
     geneNames = [...geneNames, ...geneMap.map((gene) => gene.geneName)]
@@ -35,11 +38,22 @@ export function SequenceSelectorDisconnected({ geneMap, viewedGene, setViewedGen
     [setViewedGene],
   )
 
+  const getOptionText = useCallback(
+    (gene: string) => {
+      if (gene === 'Sequence') {
+        return t('Sequence')
+      }
+
+      return t('Gene {{geneName}}', { geneName: gene })
+    },
+    [t],
+  )
+
   return (
     <select name="sequence-view-gene-dropdown" id="sequence-view-gene-dropdown" onChange={onChange} value={viewedGene}>
       {geneNames.map((gene) => (
         <option key={gene} value={gene}>
-          {gene}
+          {getOptionText(gene)}
         </option>
       ))}
     </select>
