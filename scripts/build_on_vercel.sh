67102b5148198455350382838b7a36b3bc3e2602
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-07-05 16:18:13 +0200

    chore: cleanup

--- scripts/build_on_vercel.sh
+++ scripts/build_on_vercel.sh
@@ -21,7 +21,7 @@ if [ -f "${PROJECT_ROOT_DIR}/.env" ]; then
   source "${PROJECT_ROOT_DIR}/.env"
 fi
 
-PATH=${PATH:+${PATH}:}/usr/sbin
+export PATH=/usr/sbin${PATH:+":$PATH"}
 
 # Vercel caches `node_modules/`, so let's put our caches there
 export CACHE_DIR="${PROJECT_ROOT_DIR}/node_modules"
@@ -40,99 +40,36 @@ echo "RHEL version: $(rpm -E %{rhel} || echo 'unknown')"
 
 printf "[main]\nenabled=0\n" > "/etc/yum/pluginconf.d/fastestmirror.conf"
 
+#ls -al /lib64/libvips*
 
-
-amazon-linux-extras list
-
-#amazon-linux-extras enable python3.8
-
-#yum install -y "https://centos7.iuscommunity.org/ius-release.rpm"
-
-# Enable UIS repo
-# See: https://ius.io/
-yum install -y -q \
-"https://repo.ius.io/ius-release-el7.rpm" \
-"https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
-
-
-## Enable "Extra Packages for Enterprise Linux (EPEL)".
-## See: https://fedoraproject.org/wiki/EPEL
-## Packages: https://download.fedoraproject.org/pub/epel/7/x86_64/
-#yum install -y -q "https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
-#yum install -y -q \
-#  yum-utils \
-
-#  subscription-manager \
-#subscription-manager repos --enable "rhel-*-optional-rpms" --enable "rhel-*-extras-rpms"  --enable "rhel-ha-for-rhel-*-server-rpms"
-
-yum update -y -q
-
-yum install -y -q \
-  ccache \
-  python36u python36u-libs python36u-devel python36u-pip \
-  xz \
-
-yum install -y -q --enablerepo=ius \
-  sqlite \
-  sqlite-devel \
-  python36u python36u-libs python36u-devel python36u-pip \
-
-#find / -type f -iname "python" 2>/dev/null || true
-#find / -type f -iname "python3" 2>/dev/null || true
-#find / -type f -iname "python3.6" 2>/dev/null || true
-#find / -type f -iname "python36" 2>/dev/null || true
-#
-#/python38/python -c "import sqlite3; print(sqlite3.sqlite_version)" || true
-#/python36/python -c "import sqlite3; print(sqlite3.sqlite_version)" || true
-#/usr/local/bin/python3.6 -c "import sqlite3; print(sqlite3.sqlite_version)" || true
-#/usr/bin/python3.6 -c "import sqlite3; print(sqlite3.sqlite_version)" || true
-
-
-#  python36 python36-libs python36-devel python36-pip \
-
-#  sqlite \
-#  sqlite-devel \
-#  python3.8 \
-
-#curl http://mirror.centos.org/centos/7/os/x86_64/RPM-GPG-KEY-CentOS-7 -o RPM-GPG-KEY-CentOS-7
-#rpm --import RPM-GPG-KEY-CentOS-7
-#yum-config-manager --add-repo='http://mirror.centos.org/centos/7/os/x86_64/'
-#yum install -y -q "http://mirror.centos.org/centos/7/os/x86_64/Packages/python3-3.6.8-17.el7.x86_64.rpm"
+rm /lib64/libvips-cpp.so.42
+rm /lib64/libvips.so.42
 
 mkdir -p "${CACHE_DIR}"
 mkdir -p "${DEPS_DIR}"
 
-#if [ ! -d "${CACHE_DIR}/miniconda" ]; then
-#  curl -fsSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o Miniconda3-latest-Linux-x86_64.sh
-#  bash Miniconda3-latest-Linux-x86_64.sh -b -p "${CACHE_DIR}/miniconda" >/dev/null
-#fi
-#
-#export PATH="${CACHE_DIR}/miniconda/bin:${PATH}"
-#set +x
-#source "${CACHE_DIR}/miniconda/bin/activate"
-#set -x
-#
-#conda config --add channels conda-forge
-#conda config --set channel_priority strict
-#
-#conda install --yes --quiet \
-#  conan \
 
-#pip3 install --user --upgrade \
-#  cppcheck \
-#  cpplint \
+printf "[main]\nenabled=0\n" > "/etc/yum/pluginconf.d/fastestmirror.conf"
 
+yum install -y -q \
+"https://repo.ius.io/ius-release-el7.rpm" \
+"https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
 
-curl -fsSL https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-linux-x86_64.tar.gz | tar xfz - --strip-components=1 -C "${DEPS_DIR}/"
-export PATH="${DEPS_DIR}/bin:${PATH}"
+yum update -y -q
 
-#alternatives --config python || true
+yum install -y -q --enablerepo=ius \
+  ccache \
+  which \
+  sqlite \
+  sqlite-devel \
+  python36u python36u-libs python36u-devel python36u-pip \
+  xz
+
 
 alternatives --install /usr/bin/python python /usr/bin/python3.6 60 || true
 alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 60 || true
 alternatives --install /usr/local/bin/python3 python3 /usr/bin/python3.6 60 || true
-alternatives --install /usr/bin/python3.6 python3 /usr/bin/python3.6 60 || true
-
+alternatives --install /usr/bin/python3.6 python3.6 /usr/bin/python3.6 60 || true
 
 which "python"
 python -c "import sqlite3; print(sqlite3.sqlite_version)" || true
@@ -140,15 +77,15 @@ python -c "import sqlite3; print(sqlite3.sqlite_version)" || true
 which "python3"
 python3 -c "import sqlite3; print(sqlite3.sqlite_version)" || true
 
-which "python36"
-python36 -c "import sqlite3; print(sqlite3.sqlite_version)" || true
+which "python3.6"
+python3.6 -c "import sqlite3; print(sqlite3.sqlite_version)" || true
 
+export PYTHONPATH=/usr/lib/python3.6${PYTHONPATH:+":$PYTHONPATH"}
 
+python3 -m pip install --upgrade --quiet conan
 
-#alternatives --config python || true
-
-python3 -m pip install --upgrade --quiet \
-  conan
+curl -fsSL https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-linux-x86_64.tar.gz | tar xfz - --strip-components=1 -C "${DEPS_DIR}/"
+export PATH="${DEPS_DIR}/bin:${PATH}"
 
 #export NEXTCLADE_EMSDK_DIR="${CACHE_DIR}/emscripten/emsdk-${NEXTCLADE_EMSDK_VERSION}"
 #export CONAN_USER_HOME="${CACHE_DIR}/conan"
