de2fa95869328a39f6d5bc81c23dbb4dec835875
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-07-05 12:34:09 +0200

    chore: use vercel cache

--- scripts/build_on_vercel.sh
+++ scripts/build_on_vercel.sh
@@ -11,26 +11,20 @@ trap "exit" INT
 set -x
 
 # Directory where this script resides
-THIS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") pwd)
+THIS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
 
 # Where the source code is
 PROJECT_ROOT_DIR="$(realpath ${THIS_DIR}/..)"
 
-function is_docker() {
-    if [ -f /.dockerenv ]; then
-      echo "1"
-    else
-      echo "0"
-    fi
-}
-
+source "${PROJECT_ROOT_DIR}/.env.example"
+if [ -f "${PROJECT_ROOT_DIR}/.env" ]; then
+  source "${PROJECT_ROOT_DIR}/.env"
+fi
 
 # Vercel seems to be currently using VMs provisioned with Amazon Linux, which is a derivative of RHEL,
 # so we assume that `yum` package manager and `docker` package are available.
 # If something breaks here, perhaps they've changed things.
 cat /etc/os-release
-
-IS_DOCKER="$(is_docker)"
 
 yum install -y -q \
   curl \
@@ -38,10 +32,7 @@ yum install -y -q \
 
 curl -fsSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p "${HOME}/miniconda" >/dev/null
-
-
 export PATH="${HOME}/miniconda/bin:${PATH}"
-
 set +x
 source "${HOME}/miniconda/bin/activate"
 set -x
@@ -55,6 +46,14 @@ conda install --yes --quiet \
   conan \
   cppcheck \
   cpplint \
+
+
+# Vercel caches `node_modules`, so let's put our caches there
+export NEXTCLADE_EMSDK_DIR="node_modules/emscripten/emsdk-${NEXTCLADE_EMSDK_VERSION}"
+export NEXTCLADE_EMSDK_CACHE="node_modules/emscripten/emsdk_cache-${NEXTCLADE_EMSDK_VERSION}"
+export CONAN_USER_HOME="node_modules/conan"
+export CCACHE_DIR="node_modules/ccache"
+export NEXTCLADE_EMSDK_USE_CACHE="0"
 
 make prod-wasm-nowatch
 
