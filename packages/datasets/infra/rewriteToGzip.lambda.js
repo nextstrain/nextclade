c650745eb15351cac2f56f6cb4ce52bc2fc042a2
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-07-26 09:36:48 +0200

    chore: compress dataset files on S3
    Cloudfront does not automatically compress all the file types we need and does not compress files larger than 10 Mb.
    
    Here we compress all the files manually, except for zip archives. The Viewer Request URLs are rewritten using a Lambda@Edge function to fetch the compressed content.
    (for example, if a client requests .json file, the Lambda it will make Cloudront to fetch the corresponding .json.gz file)

---
+++ packages/datasets/infra/rewriteToGzip.lambda.js
@@ -0,0 +1,15 @@
+// Implements rewrite of non-gz to gz URLs using AWS Lambda@Edge.
+//
+// Usage: Create an AWS Lambda function and attach this to "Viewer Request" event of a Cloudfront distribution
+
+exports.handler = (event, context, callback) => {
+  const request = event.Records[0].cf.request
+  const headers = request.headers
+
+  // All dataset files except .zip are gzip-compressed.
+  // Here we rewrite the URL to get the corresponding .gz files.
+  if(!request.uri.endsWith('.zip')) {
+    request.uri += '.gz'
+    callback(null, request)
+  }
+}
