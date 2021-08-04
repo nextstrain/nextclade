74403c5c39fab05fe1585ee92160aa9a3575e717
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-08-04 11:21:09 +0200

    chore: autobuild docs locally too

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,9 @@ templates_path = ['templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [
+    "README.md"
+]
 
 
 # -- Options for HTML output -------------------------------------------------
