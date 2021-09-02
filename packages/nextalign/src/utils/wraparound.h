f0ff11a14fe25c6355e40c32035cff7a723ed3f4
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-09-02 07:52:24 +0200

    feat: add int wraparound function

---
+++ packages/nextalign/src/utils/wraparound.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/**
+ * Wraps given integer `x` (positive or negative) around `period`.
+ *
+ * Examples with period=3:
+ *   6 --> 0
+ *   5 --> 2
+ *   4 --> 1
+ *   3 --> 0
+ *   2 --> 2
+ *   1 --> 1
+ *   0 --> 0
+ *  -1 --> 2
+ *  -2 --> 1
+ *  -3 --> 0
+ *  -4 --> 2
+ *  -5 --> 1
+ *  -6 --> 0
+ */
+inline int wraparound(int x, int period) {
+  if (x < 0) {
+    return ((x % period) + period) % period;
+  }
+  return x % period;
+}
