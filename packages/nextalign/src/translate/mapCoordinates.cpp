62f6bae00584b3a1f5181ea265a6bee62db7317b
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-09-07 13:42:58 +0200

    feat: make a coord map from alignment to reference (reverse coord map)

--- packages/nextalign/src/translate/mapCoordinates.cpp
+++ packages/nextalign/src/translate/mapCoordinates.cpp
@@ -9,12 +9,13 @@
 #include "utils/safe_cast.h"
 
 /**
- * Makes a map from raw coordinates to alignment coordinates
+ * Makes a map from reference coordinates to alignment coordinates
  * (Excluding the gaps in reference).
  *
  * Example:
- *   012345678901234
- *   ACTC---CGTG---A -> aln_coord = [0,1,2,3,7,8,9,10,14]
+ *   ref pos: 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
+ *   ref    : A  C  T  C  -  -  -  C  G  T  G  -  -  -  A
+ *   aln pos: 0  1  2  3           7  8  9  10          14
  */
 std::vector<int> mapCoordinates(const NucleotideSequence& ref) {
   const auto refLength = safe_cast<int>(ref.size());
@@ -25,6 +26,33 @@ std::vector<int> mapCoordinates(const NucleotideSequence& ref) {
     if (ref[i] != Nucleotide::GAP) {
       coordMap.push_back(i);
     }
+  }
+
+  coordMap.shrink_to_fit();
+  return coordMap;
+}
+
+
+/**
+ * Makes a map from alignment coordinates to reference coordinates
+ * (Excluding the gaps in reference).
+ *
+ * Example:
+ *   aln pos: 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
+ *   ref    : A  C  T  C  -  -  -  C  G  T  G  -  -  -  A
+ *   ref pos: 0  1  2  3  3  3  3  4  5  6  7  7  7  7  8
+ */
+std::vector<int> mapReverseCoordinates(const NucleotideSequence& ref) {
+  const auto refLength = safe_cast<int>(ref.size());
+
+  std::vector<int> coordMap;
+  coordMap.reserve(refLength);
+  int refPos = -1;
+  for (int i = 0; i < refLength; ++i) {
+    if (ref[i] != Nucleotide::GAP) {
+      refPos++;
+    }
+    coordMap.push_back(refPos);
   }
 
   coordMap.shrink_to_fit();
--- packages/nextalign/tests/mapCoordinates.test.cpp
+++ packages/nextalign/tests/mapCoordinates.test.cpp
@@ -1,12 +1,26 @@
+#include "../src/translate/mapCoordinates.h"
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <array>
 
-#include "../src/translate/mapCoordinates.h"
-
 TEST(mapCoordinates, MapsSimple) {
+  // ref pos: 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
+  // ref    : A  C  T  C  -  -  -  C  G  T  G  -  -  -  A
+  // aln pos: 0  1  2  3           7  8  9  10          14
+
   const auto ref = toNucleotideSequence("ACTC---CGTG---A");
-  constexpr const auto expected = std::array<int, 9>{0, 1, 2, 3, 7, 8, 9, 10, 14};
+  const auto expected = std::vector<int>{0, 1, 2, 3, 7, 8, 9, 10, 14};
   EXPECT_THAT(mapCoordinates(ref), testing::ElementsAreArray(expected));
+}
+
+TEST(mapCoordinates, MapsReverseSimple) {
+  // aln pos: 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
+  // ref    : A  C  T  C  -  -  -  C  G  T  G  -  -  -  A
+  // ref pos: 0  1  2  3  3  3  3  4  5  6  7  7  7  7  8
+
+  const auto ref = toNucleotideSequence("ACTC---CGTG---A");
+  const auto expected = std::vector<int>{0, 1, 2, 3, 3, 3, 3, 4, 5, 6, 7, 7, 7, 7, 8};
+  EXPECT_THAT(mapReverseCoordinates(ref), expected);
 }
