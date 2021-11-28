nextstrain/nextclade

commit e653ad1e36135f6049eace5f5f01c648448d95fc

refactor: use existing signed integer variable instead of .size()

packages/nextalign/src/align/alignPairwise.cpp
@@ -374,7 +374,7 @@ AlignmentStatus<Letter> backTrace(const Sequence<Letter>& query, const Sequence<
     // Determine the last index
     lastIndexByShift[i] = std::min(rowLength - 1, querySize + is);
 
-    if (lastIndexByShift[i] >= 0 && lastIndexByShift[i] < scores.num_cols()) {
+    if (lastIndexByShift[i] >= 0 && lastIndexByShift[i] < scoresSize) {
       // invariant_greater(lastIndexByShift[i], 0);
       // invariant_less(lastIndexByShift[i], scores.num_cols());
       // debug_trace(

commit 9be6f274041ceaeb8aa8718063e3155d80c2ce52

fix: use correct size variable
Bug introduced during refactoring in e653ad1e36135f6049eace5f5f01c648448d95fc

packages/nextalign/src/align/alignPairwise.cpp
@@ -369,7 +369,7 @@ inline BestAlignmentResult findBestAlignment(const vector2d<int>& scores, int ro
     const auto& index = lastIndexByShift[i];
     auto& score = lastScoreByShift[i];
 
-    if (index >= 0 && index < scoresSize) {
+    if (index >= 0 && index < rowLength) {
       score = scores(i, index);
       if (score > bestScore) {
         bestScore = score;