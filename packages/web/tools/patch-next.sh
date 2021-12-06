29250089eb81760759c6d094218831c8cad6c624
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-12-06 18:13:27 +0100

    chore(web): patch postcss to avoid deprecation message with Node.js 16

--- packages/web/tools/patch-next.sh
+++ packages/web/tools/patch-next.sh
@@ -12,3 +12,10 @@ sed -i.bak 's/warn=onWarning;/warn=function(){};/g' node_modules/next/dist/build
 # Removes "<title> should not be used in _document.js" warning.
 # Reason: We want title and other SEO tags to be pre-rendered, so that crawlers could find them.
 sed -i.bak "s|console.warn(\"Warning: <title> should not be used in _document.js's <Head>. https://err.sh/next.js/no-document-title\");||g" node_modules/next/dist/pages/_document.js
+
+
+# Removes the line with `"./": "./"` from package.json of the postcss package
+# Reason: To avoid this message when using Node.js 16:
+#     #(node:3449580) [DEP0148] DeprecationWarning: Use of deprecated folder mapping "./" in the "exports" field module resolution of the package at node_modules/css-loader/node_modules/postcss/package.json.
+#     Update this package.json to use a subpath pattern like "./*"
+sed -i.bak '/"\.\/": "\.\/"/d' "node_modules/css-loader/node_modules/postcss/package.json"
