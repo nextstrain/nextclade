53aedf4afd24504c8eff32b82952247f88378c53
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-12-21 16:32:10 +0100

    fix(web): ensure sorting of dynamic columns work

--- packages/web/src/components/Results/ResultsTable.tsx
+++ packages/web/src/components/Results/ResultsTable.tsx
@@ -400,6 +400,7 @@ export function ResultsTableDisconnected({
   cladeNodeAttrKeys,
   resultsFiltered,
   filterPanelCollapsed,
+  resultsSortByKeyTrigger,
   sortByIdAsc,
   sortByIdDesc,
   sortByNameAsc,
