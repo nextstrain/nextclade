a554fe3cd736eb6153cae4cdf8bf3d3951c4d81a
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-01-25 17:36:25 +0100

    fix: avoid underflow by using signed integers

--- packages/nextalign_rs/src/align/backtrace.rs
+++ packages/nextalign_rs/src/align/backtrace.rs
@@ -6,42 +6,42 @@ use crate::io::nuc::Nuc;
 use crate::utils::vec2d::Vec2d;
 use std::cmp;
 
-fn index_to_shift(si: usize, bandWidth: usize, meanShift: i32) -> i32 {
-  si as i32 - bandWidth as i32 + meanShift as i32
+fn index_to_shift(si: i32, bandWidth: i32, meanShift: i32) -> i32 {
+  si - bandWidth + meanShift
 }
 
 struct DetermineBestAlignmentResult {
-  si: usize,
-  qPos: usize,
-  rPos: usize,
+  si: i32,
+  qPos: i32,
+  rPos: i32,
   best_score: usize,
 }
 
 /// Determine the best alignment by picking the optimal score at the end of the query
 fn determine_best_alignment(
   scores: &Vec2d<i32>,
-  band_width: usize,
+  band_width: i32,
   mean_shift: i32,
-  qry_len: usize,
+  qry_len: i32,
 ) -> DetermineBestAlignmentResult {
-  let num_cols = scores.num_cols();
-  let num_rows = scores.num_rows();
+  let num_cols = scores.num_cols() as i32;
+  let num_rows = scores.num_rows() as i32;
 
-  let mut lastScoreByShift: Vec<i32> = vec![0; num_rows];
-  let mut lastIndexByShift: Vec<i32> = vec![0; num_rows];
+  let mut lastScoreByShift: Vec<i32> = vec![0; num_rows as usize];
+  let mut lastIndexByShift: Vec<i32> = vec![0; num_rows as usize];
 
-  let mut si = 0_usize;
+  let mut si: i32 = 0;
   let mut best_score = END_OF_SEQUENCE;
   for i in 0..num_rows {
     let is = index_to_shift(i, band_width, mean_shift);
     // Determine the last index
-    lastIndexByShift[i] = cmp::min(num_rows as i32 - 1, qry_len as i32 + is);
+    lastIndexByShift[i as usize] = cmp::min(num_rows as i32 - 1, qry_len as i32 + is);
 
-    if (lastIndexByShift[i] >= 0) && (lastIndexByShift[i] < num_cols as i32) {
-      lastScoreByShift[i] = scores[(i, lastIndexByShift[i] as usize)];
+    if (lastIndexByShift[i as usize] >= 0) && (lastIndexByShift[i as usize] < num_cols as i32) {
+      lastScoreByShift[i as usize] = scores[(i, lastIndexByShift[i as usize])];
 
-      if lastScoreByShift[i] > best_score {
-        best_score = lastScoreByShift[i];
+      if lastScoreByShift[i as usize] > best_score {
+        best_score = lastScoreByShift[i as usize];
         si = i;
       }
     }
@@ -49,10 +49,10 @@ fn determine_best_alignment(
 
   // determine position tuple qPos, rPos corresponding to the place it the matrix
   let shift = index_to_shift(si, band_width, mean_shift);
-  let rPos = (lastIndexByShift[si] - 1) as usize;
-  let qPos = rPos - shift as usize;
+  let rPos = lastIndexByShift[si as usize] - 1;
+  let qPos = rPos - shift;
   DetermineBestAlignmentResult {
-    si,
+    si: si as i32,
     qPos,
     rPos,
     best_score: best_score as usize,
@@ -74,11 +74,11 @@ pub fn backtrace(
 ) -> NextalignResult {
   let num_cols = scores.num_cols();
   let num_rows = scores.num_rows();
-  let qry_len = qry_seq.len();
-  let ref_len = ref_seq.len();
-  let bandWidth = (num_rows - 1) / 2;
+  let qry_len = qry_seq.len() as i32;
+  let ref_len = ref_seq.len() as i32;
+  let bandWidth = ((num_rows - 1) / 2) as i32;
 
-  let aln_capacity = num_cols + 3 * bandWidth;
+  let aln_capacity = num_cols + 3 * bandWidth as usize;
   let mut aln_ref = Vec::<Nuc>::with_capacity(aln_capacity);
   let mut aln_qry = Vec::<Nuc>::with_capacity(aln_capacity);
 
@@ -93,11 +93,11 @@ pub fn backtrace(
   if rPos < ref_len - 1 {
     for i in (rPos..ref_len - 1).rev() {
       aln_qry.push(Nuc::GAP);
-      aln_ref.push(ref_seq[i]);
+      aln_ref.push(ref_seq[i as usize]);
     }
   } else if qPos < qry_len - 1 {
     for i in (qPos..qry_len - 1).rev() {
-      aln_qry.push(qry_seq[i]);
+      aln_qry.push(qry_seq[i as usize]);
       aln_ref.push(Nuc::GAP);
     }
   }
@@ -111,13 +111,13 @@ pub fn backtrace(
 
     if (origin & MATCH) != 0 && (currentMatrix == 0) {
       // Match -- decrement both strands and add match to alignment
-      aln_qry.push(qry_seq[qPos]);
-      aln_ref.push(ref_seq[rPos]);
+      aln_qry.push(qry_seq[qPos as usize]);
+      aln_ref.push(ref_seq[rPos as usize]);
       qPos -= 1;
       rPos -= 1;
     } else if ((origin & refGAPmatrix) != 0 && currentMatrix == 0) || currentMatrix == refGAPmatrix {
       // Insertion in ref -- decrement query, increase shift
-      aln_qry.push(qry_seq[qPos]);
+      aln_qry.push(qry_seq[qPos as usize]);
       aln_ref.push(Nuc::GAP);
       qPos -= 1;
       si += 1;
@@ -131,7 +131,7 @@ pub fn backtrace(
     } else if ((origin & qryGAPmatrix) != 0 && currentMatrix == 0) || currentMatrix == qryGAPmatrix {
       // Deletion in query -- decrement reference, reduce shift
       aln_qry.push(Nuc::GAP);
-      aln_ref.push(ref_seq[rPos]);
+      aln_ref.push(ref_seq[rPos as usize]);
       rPos -= 1;
       si -= 1;
       currentMatrix = if (origin & qryGAPextend) != 0 {
@@ -150,11 +150,11 @@ pub fn backtrace(
   if rPos >= 0 {
     for i in (0..rPos).rev() {
       aln_qry.push(Nuc::GAP);
-      aln_ref.push(ref_seq[i]);
+      aln_ref.push(ref_seq[i as usize]);
     }
   } else if qPos >= 0 {
     for i in (0..qPos).rev() {
-      aln_qry.push(qry_seq[i]);
+      aln_qry.push(qry_seq[i as usize]);
       aln_ref.push(Nuc::GAP);
     }
   }
--- packages/nextalign_rs/src/utils/vec2d.rs
+++ packages/nextalign_rs/src/utils/vec2d.rs
@@ -29,6 +29,12 @@ impl<T: Default + Clone> Vec2d<T> {
     let (row, col) = index2d;
     row * self.n_cols + col
   }
+
+  #[inline]
+  fn get_index_i32(&self, index2d: (i32, i32)) -> usize {
+    let (row, col) = index2d;
+    (row * self.n_cols as i32 + col) as usize
+  }
 }
 
 /// Allows 2-dimensional indexing using a tuple:
@@ -47,5 +53,24 @@ impl<T: Default + Clone> Index<(usize, usize)> for Vec2d<T> {
 impl<T: Default + Clone> IndexMut<(usize, usize)> for Vec2d<T> {
   fn index_mut(&mut self, index2d: (usize, usize)) -> &mut Self::Output {
     self.data.index_mut(self.get_index(index2d))
+  }
+}
+
+/// Allows 2-dimensional indexing using a tuple:
+///  arr[(row, col)]
+impl<T: Default + Clone> Index<(i32, i32)> for Vec2d<T> {
+  type Output = T;
+  #[inline]
+
+  fn index(&self, index2d: (i32, i32)) -> &Self::Output {
+    self.data.index(self.get_index_i32(index2d))
+  }
+}
+
+/// Allows 2-dimensional mutable indexing using a tuple:
+///  arr[(row, col)]
+impl<T: Default + Clone> IndexMut<(i32, i32)> for Vec2d<T> {
+  fn index_mut(&mut self, index2d: (i32, i32)) -> &mut Self::Output {
+    self.data.index_mut(self.get_index_i32(index2d))
   }
 }
