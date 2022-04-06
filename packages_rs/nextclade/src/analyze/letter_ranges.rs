c1df59489bb4cb7b2fa9fcb542153fa0fcd7037f
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-04-06 13:52:26 +0200

    fix: don't emit empty aa ranges

--- packages_rs/nextclade/src/analyze/letter_ranges.rs
+++ packages_rs/nextclade/src/analyze/letter_ranges.rs
@@ -119,15 +119,15 @@ impl GeneAaRange {
 /// Finds contiguous ranges (segments) consisting of a given amino acid in the sequence.
 pub fn find_aa_letter_ranges(translations: &[Result<Translation, Report>], letter: Aa) -> Vec<GeneAaRange> {
   keep_ok(translations)
-    .map(|Translation { gene_name, seq, .. }| {
+    .filter_map(|Translation { gene_name, seq, .. }| {
       let ranges = find_letter_ranges_by(seq, |candidate| candidate == letter);
       let length = ranges.iter().map(LetterRange::len).sum();
-      GeneAaRange {
+      (length > 0).then(|| GeneAaRange {
         gene_name: gene_name.clone(),
         letter,
         ranges,
         length,
-      }
+      })
     })
     .collect()
 }
