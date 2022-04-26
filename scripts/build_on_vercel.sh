79428fc662e09eabf375b8e1e640ddd01c2e5272
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-04-26 16:46:44 +0200

    chore(ci): setup vercel build

--- scripts/build_on_vercel.sh
+++ scripts/build_on_vercel.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-BASH_DEBUG="${BASH_DEBUG:=}"
-([ "${BASH_DEBUG}" == "true" ] || [ "${BASH_DEBUG}" == "1" ]) && set -o xtrace
-set -o errexit
-set -o nounset
-set -o pipefail
-shopt -s dotglob
+set -euxo pipefail
 trap "exit" INT
 
-set -x
-
 # Directory where this script resides
-THIS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+THIS_DIR="$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd
+)"
 
 # Where the source code is
 PROJECT_ROOT_DIR="$(realpath ${THIS_DIR}/..)"
@@ -40,7 +36,7 @@ cat /etc/system-release
 echo "RHEL version: $(rpm -E %{rhel} || echo 'unknown')"
 
 # Disable yum fastestmirror plugin. It only makes things slower.
-printf "[main]\nenabled=0\n" > "/etc/yum/pluginconf.d/fastestmirror.conf"
+printf "[main]\nenabled=0\n" >"/etc/yum/pluginconf.d/fastestmirror.conf"
 
 # Remove some dead symlinks which cause log pollution
 rm /lib64/libvips-cpp.so.42
@@ -52,11 +48,21 @@ rm -rf /usr/local/lib/python3.9 /usr/local/include/python3.9 /usr/local/bin/pyth
 
 rm -rf /usr/local/bin/python3
 
+yum clean all
+
+rm -rf /var/cache/yum/*
+
+yum makecache
+
+rpm --rebuilddb
+
 # Add "UIS" repos, with more up-to-date packages (https://ius.io/)
 # Python 3.6 from IUS has SQLite enabled.
 yum install -y -q \
-"https://repo.ius.io/ius-release-el7.rpm" \
-"https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
+  "https://repo.ius.io/ius-release-el7.rpm" \
+  "https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
+
+yum update -y -q
 
 yum update -y -q
 
@@ -76,36 +82,55 @@ ln -s /usr/bin/python3.6 /usr/local/bin/python3.6 || true
 ln -s /usr/lib/python3.6 /usr/local/lib/python3.6 || true
 export PYTHONPATH=/usr/lib/python3.6${PYTHONPATH:+":$PYTHONPATH"}
 
-
-find / -iname "python" 2>/dev/null || true
-find / -iname "python3" 2>/dev/null || true
-find / -iname "python3.6" 2>/dev/null || true
-find / -iname "python3.9" 2>/dev/null || true
-
-
-
-which "python" || true
-which "python3" || true
-which "python3.6" || true
-which "python3.9" || true
-
-
-# Make sure Python has SQLite module enabled
-which "python3"
-python3 -c "import sqlite3; print(sqlite3.sqlite_version)" || true
-
-python3 -m pip install --upgrade --quiet conan
-
 # Install CMake
 curl -fsSL https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-linux-x86_64.tar.gz | tar xfz - --strip-components=1 -C "${DEPS_DIR}/"
 export PATH="${DEPS_DIR}/bin:${PATH}"
 
-# `ccache` produces relatively small cache, let's keep it
-export CCACHE_DIR="${CACHE_DIR}/ccache"
+DASEL_VERSION="1.22.1"
+curl -fsSL "https://github.com/TomWright/dasel/releases/download/v${DASEL_VERSION}/dasel_linux_amd64" -o "/usr/bin/dasel"
+chmod +x "/usr/bin/dasel"
 
-# `emsdk` cache is too big to fit to Vercel's 500MB allowed, so don't cache.
-export NEXTCLADE_EMSDK_USE_CACHE="0"
+export CARGO_HOME="${HOME}/.cargo"
+export CARGO_INSTALL_ROOT="${HOME}/.cargo/install"
+export RUSTUP_HOME="${HOME}/.rustup"
+export PATH="/usr/lib/llvm-13/bin:${HOME}/.local/bin:${HOME}/.cargo/bin:${HOME}/.cargo/install/bin:${PATH}"
 
-make prod-wasm-nowatch
+# Install rustup and toolchain from rust-toolchain.toml
+RUST_TOOLCHAIN=$(dasel select -p toml -s ".toolchain.channel" -f "rust-toolchain.toml")
+curl -sSf https://sh.rustup.rs >rustup-init
+chmod +x rustup-init
+./rustup-init -y --no-modify-path --default-toolchain="${RUST_TOOLCHAIN}"
+rustup toolchain install "${HOME}"
+rustup default "${RUST_TOOLCHAIN}"
+rustup target add wasm32-unknown-unknown
 
-make prod-web-nowatch
+# Install cargo-quickinstall
+export CARGO_QUICKINSTALL_VERSION="0.2.6"
+curl -sSL "https://github.com/alsuren/cargo-quickinstall/releases/download/cargo-quickinstall-${CARGO_QUICKINSTALL_VERSION}-x86_64-unknown-linux-musl/cargo-quickinstall-${CARGO_QUICKINSTALL_VERSION}-x86_64-unknown-linux-musl.tar.gz" | tar -C "${CARGO_HOME}/bin" -xz "cargo-quickinstall"
+chmod +x "${CARGO_HOME}/bin/cargo-quickinstall"
+
+# Install wasm-bindgen
+export WASM_BINDGEN_CLI_VERSION="0.2.80"
+curl -sSL "https://github.com/rustwasm/wasm-bindgen/releases/download/${WASM_BINDGEN_CLI_VERSION}/wasm-bindgen-${WASM_BINDGEN_CLI_VERSION}-x86_64-unknown-linux-musl.tar.gz" | tar -C "${CARGO_HOME}/bin" --strip-components=1 -xz "wasm-bindgen-${WASM_BINDGEN_CLI_VERSION}-x86_64-unknown-linux-musl/wasm-bindgen"
+chmod +x "${CARGO_HOME}/bin/wasm-bindgen"
+
+
+export WASM_PACK_VERSION="0.10.2"
+curl -sSL "https://github.com/rustwasm/wasm-pack/releases/download/v${WASM_PACK_VERSION}/wasm-pack-v${WASM_PACK_VERSION}-x86_64-unknown-linux-musl.tar.gz" | tar -C "${CARGO_HOME}/bin" --strip-components=1 -xz "wasm-pack-v${WASM_PACK_VERSION}-x86_64-unknown-linux-musl/wasm-pack"
+chmod +x "${CARGO_HOME}/bin/wasm-pack"
+
+
+# Install executable dependencies
+cargo quickinstall cargo-generate
+
+cp ".env.example" ".env"
+
+cd packages_rs/nextclade-web
+
+yarn wasm-prod
+
+yarn install --frozen-lockfile
+
+yarn prod:build
+
+cp -r "packages_rs/nextclade-web/.build/production/web" "packages/web/.build/production/web"
