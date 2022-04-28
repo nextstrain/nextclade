c7cb04af2a24ae30f615ed362648c6e310ab45f2
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-04-28 14:58:07 +0200

    chore(ci): cleanup vercel build script

--- scripts/build_on_vercel.sh
+++ scripts/build_on_vercel.sh
@@ -17,83 +17,50 @@ if [ -f "${PROJECT_ROOT_DIR}/.env" ]; then
   source "${PROJECT_ROOT_DIR}/.env"
 fi
 
-export PATH=/usr/sbin${PATH:+":$PATH"}
-
-# Vercel caches `node_modules/`, so let's put our caches there
-export CACHE_DIR="${PROJECT_ROOT_DIR}/node_modules"
-
-# Deps that are not cached
-export DEPS_DIR="${PROJECT_ROOT_DIR}/deps"
-
-mkdir -p "${CACHE_DIR}"
-mkdir -p "${DEPS_DIR}"
-
 # Vercel seems to be currently using VMs provisioned with Amazon Linux 2, which is a derivative of RHEL 7,
 # If something breaks, perhaps they've changed things.
 cat /etc/os-release
 cat /etc/image-id
 cat /etc/system-release
-echo "RHEL version: $(rpm -E %{rhel} || echo 'unknown')"
+echo "RHEL version: $(rpm -E '%{rhel}' || echo 'unknown')"
 
 # Disable yum fastestmirror plugin. It only makes things slower.
 printf "[main]\nenabled=0\n" >"/etc/yum/pluginconf.d/fastestmirror.conf"
 
-# Remove some dead symlinks which cause log pollution
-rm /lib64/libvips-cpp.so.42
-rm /lib64/libvips.so.42
-
-# Remove default Python. It does not have SQLite module enabled required for `conan`.
-rm -rf /usr/local/bin/python3.6 /usr/local/lib/python3.6
-rm -rf /usr/local/lib/python3.9 /usr/local/include/python3.9 /usr/local/bin/python3.9
-
-rm -rf /usr/local/bin/python3
-
-yum clean all
-
-rm -rf /var/cache/yum/*
-
-yum makecache
-
-rpm --rebuilddb
-
-# Add "UIS" repos, with more up-to-date packages (https://ius.io/)
-# Python 3.6 from IUS has SQLite enabled.
+#yum clean all
+#rm -rf /var/cache/yum/*
+#yum makecache
+#yum update -y -q
 yum install -y -q \
-  "https://repo.ius.io/ius-release-el7.rpm" \
-  "https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
-
-yum update -y -q
-
-yum update -y -q
-
-yum install -y -q --enablerepo=ius \
-  ccache \
-  sqlite \
-  sqlite-devel \
-  python36u python36u-libs python36u-devel python36u-pip \
+  gzip \
+  tar \
   xz
 
-# Set Python 3.6 from UIS as  default
-alternatives --install /usr/bin/python python /usr/bin/python3.6 60 || true
-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 60 || true
-alternatives --install /usr/local/bin/python3 python3 /usr/bin/python3.6 60 || true
-alternatives --install /usr/bin/python3.6 python3.6 /usr/bin/python3.6 60 || true
-ln -s /usr/bin/python3.6 /usr/local/bin/python3.6 || true
-ln -s /usr/lib/python3.6 /usr/local/lib/python3.6 || true
-export PYTHONPATH=/usr/lib/python3.6${PYTHONPATH:+":$PYTHONPATH"}
+# Remove some dead symlinks which cause log pollution
+rm -f /lib64/libvips-cpp.so.42
+rm -f /lib64/libvips.so.42
 
-# Install CMake
-curl -fsSL https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-linux-x86_64.tar.gz | tar xfz - --strip-components=1 -C "${DEPS_DIR}/"
-export PATH="${DEPS_DIR}/bin:${PATH}"
+# Vercel caches `node_modules/`, so let's put our caches there
+export CACHE_DIR="${PROJECT_ROOT_DIR}/node_modules/.cache"
 
-DASEL_VERSION="1.22.1"
-curl -fsSL "https://github.com/TomWright/dasel/releases/download/v${DASEL_VERSION}/dasel_linux_amd64" -o "/usr/bin/dasel"
-chmod +x "/usr/bin/dasel"
+mkdir -p "${CACHE_DIR}"
 
 export CARGO_HOME="${HOME}/.cargo"
 export CARGO_INSTALL_ROOT="${HOME}/.cargo/install"
 export RUSTUP_HOME="${HOME}/.rustup"
-export PATH="/usr/lib/llvm-13/bin:${HOME}/.local/bin:${HOME}/.cargo/bin:${HOME}/.cargo/install/bin:${PATH}"
+export PATH=/usr/lib/llvm-13/bin:${HOME}/.local/bin:${HOME}/.cargo/bin:${HOME}/.cargo/install/bin:/usr/sbin${PATH:+":$PATH"}
+
+
+mkdir -p "${CACHE_DIR}/.cargo"
+ln -s "${CACHE_DIR}/.cargo" "${HOME}/.cargo"
+
+mkdir -p "${CACHE_DIR}/.build"
+ln -s "${CACHE_DIR}/.build" "target"
+
+# Install dasel
+DASEL_VERSION="1.22.1"
+curl -fsSL "https://github.com/TomWright/dasel/releases/download/v${DASEL_VERSION}/dasel_linux_amd64" -o "/usr/bin/dasel"
+chmod +x "/usr/bin/dasel"
 
 # Install rustup and toolchain from rust-toolchain.toml
 RUST_TOOLCHAIN=$(dasel select -p toml -s ".toolchain.channel" -f "rust-toolchain.toml")
@@ -114,14 +81,10 @@ export WASM_BINDGEN_CLI_VERSION="0.2.80"
 curl -sSL "https://github.com/rustwasm/wasm-bindgen/releases/download/${WASM_BINDGEN_CLI_VERSION}/wasm-bindgen-${WASM_BINDGEN_CLI_VERSION}-x86_64-unknown-linux-musl.tar.gz" | tar -C "${CARGO_HOME}/bin" --strip-components=1 -xz "wasm-bindgen-${WASM_BINDGEN_CLI_VERSION}-x86_64-unknown-linux-musl/wasm-bindgen"
 chmod +x "${CARGO_HOME}/bin/wasm-bindgen"
 
-
+# Install wasm-pack
 export WASM_PACK_VERSION="0.10.2"
 curl -sSL "https://github.com/rustwasm/wasm-pack/releases/download/v${WASM_PACK_VERSION}/wasm-pack-v${WASM_PACK_VERSION}-x86_64-unknown-linux-musl.tar.gz" | tar -C "${CARGO_HOME}/bin" --strip-components=1 -xz "wasm-pack-v${WASM_PACK_VERSION}-x86_64-unknown-linux-musl/wasm-pack"
 chmod +x "${CARGO_HOME}/bin/wasm-pack"
-
-
-# Install executable dependencies
-cargo quickinstall cargo-generate
 
 cp ".env.example" ".env"
 
