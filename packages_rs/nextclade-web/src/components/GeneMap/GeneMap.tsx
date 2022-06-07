5ad4bdda94a0024d242a7af4e19ab9117a771e81
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-06-07 11:21:15 +0200

    feat(web): combine gene offsets for frame and for strand in gene map

--- packages_rs/nextclade-web/src/components/GeneMap/GeneMap.tsx
+++ packages_rs/nextclade-web/src/components/GeneMap/GeneMap.tsx
@@ -1,4 +1,5 @@
-import React, { SVGProps, useCallback, useEffect, useState } from 'react'
+import { groupBy } from 'lodash'
+import React, { SVGProps, useCallback, useEffect, useMemo, useState } from 'react'
 import { ReactResizeDetectorDimensions, withResizeDetector } from 'react-resize-detector'
 import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { geneLength } from 'src/algorithms/types'
@@ -16,15 +17,43 @@ import { ColoredSquare } from 'src/components/Common/ColoredSquare'
 import { geneMapAtom, genomeSizeAtom } from 'src/state/results.state'
 import { viewedGeneAtom } from 'src/state/settings.state'
 
-export const GENE_MAP_HEIGHT_PX = 35
-export const GENE_HEIGHT_PX = 15
-export const GENE_STRAND_SHIFT = 5
-export const geneMapY = -GENE_MAP_HEIGHT_PX / 2
+const GENE_MAP_MARGIN = 5
+const GENE_HEIGHT_PX = 15
+const GENE_STRAND_SPACING = 10
 
-export const GeneMapWrapper = styled.div`
+export function getGeneMapDimensions(genes: Gene[]) {
+  const frames = Object.keys(groupBy(genes, (gene) => gene.frame))
+  const hasFrames = frames.length > 1
+
+  const strands = Object.keys(groupBy(genes, (gene) => gene.strand))
+  const hasStrands = strands.length > 1
+
+  const geneFrameOffset = GENE_HEIGHT_PX / 2
+  let geneStrandOffset = geneFrameOffset * 3
+
+  let geneMapHeight = GENE_MAP_MARGIN + GENE_HEIGHT_PX
+  if (hasFrames && hasStrands) {
+    geneMapHeight += geneFrameOffset * 3 + geneStrandOffset + GENE_STRAND_SPACING
+    geneStrandOffset += GENE_STRAND_SPACING
+  } else if (hasStrands) {
+    geneMapHeight += geneStrandOffset + GENE_STRAND_SPACING
+  } else if (hasFrames) {
+    geneMapHeight += geneFrameOffset * 3
+  } else {
+    geneMapHeight += geneFrameOffset * 3
+  }
+
+  return {
+    geneFrameOffset,
+    geneStrandOffset,
+    geneMapHeight,
+  }
+}
+
+export const GeneMapWrapper = styled.div<{ $height: number }>`
   display: flex;
   width: 100%;
-  height: ${GENE_MAP_HEIGHT_PX}px;
+  height: ${(props) => props.$height}px;
   padding: 0;
   margin: 0 auto;
 `
@@ -40,9 +69,11 @@ export interface GeneViewProps extends SVGProps<SVGRectElement> {
   gene: Gene
   single: boolean
   pixelsPerBase: number
+  geneFrameOffset: number
+  geneStrandOffset: number
 }
 
-export function GeneView({ gene, single, pixelsPerBase, ...rest }: GeneViewProps) {
+export function GeneView({ gene, single, pixelsPerBase, geneFrameOffset, geneStrandOffset, ...rest }: GeneViewProps) {
   const { t } = useTranslationSafe()
   const [showTooltip, setShowTooltip] = useState(false)
 
@@ -78,13 +109,17 @@ export function GeneView({ gene, single, pixelsPerBase, ...rest }: GeneViewProps
   const openTooltip = useCallback(() => setHoveredSmart(true), [setHoveredSmart])
   const closeTooltip = useCallback(() => setHoveredSmart(false), [setHoveredSmart])
 
-  const { geneName, color, start, end, frame } = gene // prettier-ignore
+  const { geneName, color, start, end, frame, strand } = gene // prettier-ignore
   const length = geneLength(gene)
   const width = Math.max(BASE_MIN_WIDTH_PX, length * pixelsPerBase)
   const x = single ? 0 : start * pixelsPerBase
   const id = getSafeId('gene', { ...gene })
-  const strand = gene.strand === '+' ? 0 : 1
   const stroke = hovered ? '#222' : undefined
+
+  let y = geneFrameOffset * frame
+  if (strand === '-') {
+    y += geneStrandOffset
+  }
 
   const onClick = useCallback(() => {
     clearInterval(timeoutId)
@@ -97,9 +132,9 @@ export function GeneView({ gene, single, pixelsPerBase, ...rest }: GeneViewProps
       id={id}
       fill={color}
       x={x}
-      y={-10 + 7.5 * frame + GENE_STRAND_SHIFT * strand}
+      y={y}
       width={width}
-      height={GENE_HEIGHT_PX - GENE_STRAND_SHIFT}
+      height={GENE_HEIGHT_PX}
       onMouseEnter={openTooltip}
       onMouseLeave={closeTooltip}
       onClick={onClick}
@@ -121,7 +156,7 @@ export function GeneView({ gene, single, pixelsPerBase, ...rest }: GeneViewProps
 
             <tr>
               <td>{t('Strand:')}</td>
-              <td>{gene.strand}</td>
+              <td>{strand}</td>
             </tr>
 
             <tr>
@@ -158,43 +193,57 @@ export function GeneView({ gene, single, pixelsPerBase, ...rest }: GeneViewProps
 
 export type GeneMapProps = ReactResizeDetectorDimensions
 
-export function GeneMapUnsized({ width, height }: GeneMapProps) {
+export function GeneMapUnsized({ width = 0, height = 0 }: GeneMapProps) {
   const geneMap = useRecoilValue(geneMapAtom)
   const genomeSize = useRecoilValue(genomeSizeAtom)
   const viewedGene = useRecoilValue(viewedGeneAtom)
 
+  const { viewBox, geneViews, geneMapHeight } = useMemo(() => {
+    const length = getAxisLength(genomeSize, viewedGene, geneMap)
+    const pixelsPerBase = width / length
+    const single = viewedGene !== GENE_OPTION_NUC_SEQUENCE
+
+    let geneMapFiltered: Gene[] = []
+    if (!single) {
+      geneMapFiltered = geneMap
+    } else {
+      const gene = geneMap.find((gene) => gene.geneName === viewedGene)
+      if (gene) {
+        geneMapFiltered = [gene]
+      }
+    }
+
+    const { geneFrameOffset, geneStrandOffset, geneMapHeight } = getGeneMapDimensions(geneMapFiltered)
+
+    const geneViews = geneMapFiltered.map((gene) => {
+      return (
+        <GeneView
+          key={gene.geneName}
+          gene={gene}
+          single={single}
+          pixelsPerBase={pixelsPerBase}
+          geneFrameOffset={geneFrameOffset}
+          geneStrandOffset={geneStrandOffset}
+        />
+      )
+    })
+
+    const viewBox = `0 ${-GENE_MAP_MARGIN} ${width} ${geneMapHeight}`
+
+    return { viewBox, geneViews, geneMapHeight }
+  }, [geneMap, genomeSize, viewedGene, width])
+
   if (!width || !height) {
     return (
-      <GeneMapWrapper>
-        <GeneMapSVG viewBox={`0 -25 50 50`} />
+      <GeneMapWrapper $height={1}>
+        <GeneMapSVG viewBox={`0 0 50 50`} />
       </GeneMapWrapper>
     )
   }
 
-  const length = getAxisLength(genomeSize, viewedGene, geneMap)
-  const pixelsPerBase = width / length
-  const single = viewedGene !== GENE_OPTION_NUC_SEQUENCE
-
-  let geneMapFiltered: Gene[] = []
-  if (!single) {
-    geneMapFiltered = geneMap
-  } else {
-    const gene = geneMap.find((gene) => gene.geneName === viewedGene)
-    if (gene) {
-      geneMapFiltered = [gene]
-    }
-  }
-
-  const geneViews = geneMapFiltered.map((gene) => {
-    return <GeneView key={gene.geneName} gene={gene} single={single} pixelsPerBase={pixelsPerBase} />
-  })
-
   return (
-    <GeneMapWrapper>
-      <GeneMapSVG viewBox={`0 ${geneMapY} ${width} ${GENE_MAP_HEIGHT_PX}`}>
-        <rect fill="transparent" x={0} y={geneMapY} width={width} height={GENE_MAP_HEIGHT_PX} />
-        {geneViews}
-      </GeneMapSVG>
+    <GeneMapWrapper $height={geneMapHeight}>
+      <GeneMapSVG viewBox={viewBox}>{geneViews}</GeneMapSVG>
     </GeneMapWrapper>
   )
 }
