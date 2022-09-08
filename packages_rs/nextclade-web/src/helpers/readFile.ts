7ea8414fc67e6bf1a28b0b6d31404e8ac0504f8e
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-09-08 18:32:31 +0200

    fix: consider different file encodings
    This attempts to mitigate crash in web when the fasta file is not UTF-8 or ASCII encoded.
    
    Here I am trying to deduce a list of encodings the file resembles and then attempting to decode the contents of the file from these encodings to a JS string, instead of simply using the `FileReader`'s `.toString()` function.

--- packages_rs/nextclade-web/src/helpers/readFile.ts
+++ packages_rs/nextclade-web/src/helpers/readFile.ts
@@ -1,7 +1,30 @@
+/* eslint-disable no-loops/no-loops */
+import chardet from 'chardet'
+import { sortBy } from 'lodash'
+import { sanitizeError } from 'src/helpers/sanitizeError'
+
 export class FileReaderError extends Error {
   public readonly file: File
-  constructor(file: File) {
-    super(`Error: file "${file.name}" cannot be read.`)
+
+  constructor(file: File, message?: string) {
+    let err = `File cannot be read: "${file.name}" (size: ${file?.size ?? 0} bytes)`
+    if (message) {
+      err = `File cannot be read: ${message}: "${file.name}" (size: ${file?.size ?? 0} bytes)`
+    }
+    super(err)
+    this.file = file
+  }
+}
+
+export class FileReaderEncodingError extends FileReaderError {
+  public readonly file: File
+
+  constructor(file: File, message?: string) {
+    let err = `Please try to convert your file to ASCII or UTF-8 and try again!`
+    if (message) {
+      err = `${message}. ${err}`
+    }
+    super(file, err)
     this.file = file
   }
 }
@@ -10,20 +33,50 @@ export function readFile(file: File): Promise<string> {
   const reader = new FileReader()
 
   return new Promise((resolve, reject) => {
-    reader.addEventListener('error', () => {
+    reader.addEventListener('error', (event) => {
       reader.abort()
-      reject(new FileReaderError(file))
+      const message = event.target?.error?.message
+      reject(new FileReaderError(file, message))
     })
 
-    reader.addEventListener('load', () => {
-      const content = reader.result?.toString?.()
-      if (content) {
-        resolve(content)
-      } else {
-        reject(new FileReaderError(file))
+    reader.addEventListener('load', (event) => {
+      const buf = event?.target?.result
+      const isArrayBuffer = buf instanceof ArrayBuffer
+      if (!buf || !isArrayBuffer) {
+        return reject(new FileReaderError(file, 'Result is empty'))
       }
+      const bytes = new Uint8Array(buf)
+      const encodings = sortBy(chardet.analyse(bytes), (enc) => -enc.confidence)
+
+      if (encodings.length === 0) {
+        return reject(new FileReaderEncodingError(file, 'Unable detect file encoding'))
+      }
+
+      for (const { name: encoding } of encodings) {
+        try {
+          const decoder = new TextDecoder(encoding)
+          const content = decoder.decode(bytes)
+
+          if (!content || !content?.length) {
+            return reject(new FileReaderEncodingError(file, 'File contents is empty'))
+          }
+
+          return resolve(content)
+        } catch (error_: unknown) {
+          const error = sanitizeError(error_)
+          console.warn(`When converting file encoding from ${encoding} to UTF-8: ${error.message}`)
+        }
+      }
+
+      const encodingsList = encodings.map((enc) => enc.name).join(', ')
+      return reject(
+        new FileReaderEncodingError(
+          file,
+          `Unable detect file encoding. Attempted to convert from the following encodings: ${encodingsList}`,
+        ),
+      )
     })
 
-    reader.readAsText(file)
+    reader.readAsArrayBuffer(file)
   })
 }
