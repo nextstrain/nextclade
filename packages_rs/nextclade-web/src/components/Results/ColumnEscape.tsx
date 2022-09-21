ac1b14a315c878c8c6b2ca4b3d3e22dcf99c05f7
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-09-21 05:56:19 +0200

    feat(web): add tooltips for escape column

--- packages_rs/nextclade-web/src/components/Results/ColumnEscape.tsx
+++ packages_rs/nextclade-web/src/components/Results/ColumnEscape.tsx
@@ -1,7 +1,11 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
+import { Col, Row } from 'reactstrap'
 
 import type { AnalysisResult } from 'src/types'
 import { getSafeId } from 'src/helpers/getSafeId'
+import { Tooltip } from 'src/components/Results/Tooltip'
+import { useTranslationSafe } from 'src/helpers/useTranslationSafe'
+import { TableSlimWithBorders } from 'src/components/Common/TableSlim'
 
 export interface ColumnEscapeProps {
   analysisResult: AnalysisResult
@@ -9,6 +13,9 @@ export interface ColumnEscapeProps {
 
 export function ColumnEscape({ analysisResult }: ColumnEscapeProps) {
   const { index, seqName, escape } = analysisResult
+  const [showTooltip, setShowTooltip] = useState(false)
+  const onMouseEnter = useCallback(() => setShowTooltip(true), [])
+  const onMouseLeave = useCallback(() => setShowTooltip(false), [])
   const id = getSafeId('col-escape', { index, seqName })
 
   const escapePercentage = useMemo(() => {
@@ -20,12 +27,56 @@ export function ColumnEscape({ analysisResult }: ColumnEscapeProps) {
   }, [escape])
 
   return (
-    <div id={id} className="w-100">
+    <div id={id} className="w-100" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       {escapePercentage}
+      <Tooltip id={id} isOpen={showTooltip} target={id} wide fullWidth>
+        <Row noGutters>
+          <Col>
+            <ListOfEscapes escapes={escape} />
+          </Col>
+        </Row>
+      </Tooltip>
     </div>
   )
 }
 
 function formatEscapeValue(escape: number) {
   return `${escape.toFixed(4)}`
+}
+
+export interface ListOfEscapesProps {
+  escapes: Record<string, number>
+}
+
+export function ListOfEscapes({ escapes }: ListOfEscapesProps) {
+  const { t } = useTranslationSafe()
+
+  const { thead, tbody } = useMemo(() => {
+    const thead = (
+      <tr>
+        <th className="text-center">{t('Name')}</th>
+        <th className="text-center">{t('Escape')}</th>
+      </tr>
+    )
+
+    const tbody = Object.entries(escapes).map(([name, escape]) => (
+      <tr key={name}>
+        <td className="text-center">{name}</td>
+        <td className="text-center">{formatEscapeValue(escape)}</td>
+      </tr>
+    ))
+
+    return { thead, tbody }
+  }, [escapes, t])
+
+  if (Object.entries(escapes).length === 0) {
+    return null
+  }
+
+  return (
+    <TableSlimWithBorders>
+      <thead>{thead}</thead>
+      <tbody>{tbody}</tbody>
+    </TableSlimWithBorders>
+  )
 }
