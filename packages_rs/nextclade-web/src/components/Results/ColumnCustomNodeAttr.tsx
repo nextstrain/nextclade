bf85198f1fb9e3424d949cae4772c2ea901656da
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-09-22 15:18:39 +0200

    feat(web): center phenotype value

--- packages_rs/nextclade-web/src/components/Results/ColumnCustomNodeAttr.tsx
+++ packages_rs/nextclade-web/src/components/Results/ColumnCustomNodeAttr.tsx
@@ -12,17 +12,18 @@ export interface ColumnCustomNodeAttrProps {
 
 export function ColumnCustomNodeAttr({ sequence, attrKey }: ColumnCustomNodeAttrProps) {
   const { index, seqName, customNodeAttributes, phenotypeValues } = sequence
-  const { id, attrValue } = useMemo(() => {
+  const { id, attrValue, className } = useMemo(() => {
     const customCladeValue = get(customNodeAttributes, attrKey)
     const phenotypeValue = phenotypeValues?.find((ph) => ph.name === attrKey)?.value.toFixed(2)
     const id = getSafeId('col-custom-attr', { index, seqName, attrKey })
     const attrValue = customCladeValue ?? phenotypeValue ?? ''
-    return { id, attrValue }
+    const className = phenotypeValue ? 'mx-auto' : ''
+    return { id, attrValue, className }
   }, [attrKey, customNodeAttributes, index, phenotypeValues, seqName])
 
   return (
-    <div id={id} className="w-100">
-      {attrValue}
+    <div id={id} className="w-100 d-flex">
+      <span className={className}>{attrValue}</span>
     </div>
   )
 }
