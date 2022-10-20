327066aee9d3e0939ff652b35644f2d67cb5011c
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-10-20 06:39:39 +0200

    perf: use jemalloc
    Let's try jemaloc memory allocator
    
    See: https://github.com/nextstrain/nextclade/issues/1027

--- packages_rs/nextclade-cli/src/bin/nextclade.rs
+++ packages_rs/nextclade-cli/src/bin/nextclade.rs
@@ -3,9 +3,9 @@ use eyre::Report;
 use nextclade::utils::global_init::global_init;
 use nextclade_cli::cli::nextclade_cli::nextclade_parse_cli_args;
 
-#[cfg(all(target_family = "linux", target_arch = "x86_64"))]
+#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 #[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[ctor]
 fn init() {
