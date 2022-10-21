5fd91dc7f4246934ca70b9234a102bf233ab2817
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-10-21 08:29:12 +0200

    refactor: remove duplicated import

--- packages_rs/nextclade-web/src/components/Layout/NavigationBar.tsx
+++ packages_rs/nextclade-web/src/components/Layout/NavigationBar.tsx
@@ -1,5 +1,4 @@
 import React, { useMemo } from 'react'
-import { useTranslationSafe as useTranslation } from 'src/helpers/useTranslationSafe'
 
 import { FaDocker, FaGithub, FaTwitter } from 'react-icons/fa'
 import { IoMdBook } from 'react-icons/io'
@@ -59,7 +58,7 @@ export function DocsLink() {
 }
 
 export function NavigationBar() {
-  const { t } = useTranslation()
+  const { t } = useTranslationSafe()
 
   const navLinksRight = useMemo(
     () => [
