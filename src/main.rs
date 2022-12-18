70fbe713ec2b1bba3870aec2e33876a4f095d88c
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-12-18 16:55:06 +0100

    Wrap FM index into struct with methods
    This abstracts the implementation details away,
    which means high level logic is easier to follow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bio::alphabets;
-use bio::data_structures::bwt::{bwt, less, Occ};
+use bio::data_structures::bwt::{bwt, less, Less, Occ, BWT};
 use bio::data_structures::fmindex::{BackwardSearchResult, FMIndex, FMIndexable};
-use bio::data_structures::suffix_array::suffix_array;
+use bio::data_structures::suffix_array::{suffix_array, RawSuffixArray};
 use bio::io::fasta;
 use bio::io::fasta::Records;
 use std::fs::File;
@@ -14,6 +14,62 @@ fn read_fasta(filename: &str) -> Records<BufReader<File>> {
     reader.records()
 }
 
+struct Index {
+    fm_index: FMIndex<BWT, Less, Occ>,
+    suffix_array: RawSuffixArray,
+}
+
+impl Index {
+    /// Creates a new FMindex from a reference sequence
+    fn from_sequence(reference: &[u8]) -> Self {
+        let alphabet = alphabets::dna::iupac_alphabet();
+
+        let mut reference_owned = reference.to_owned();
+
+        //Need to end the sequence that's indexed with the special/magic character `$`
+        //otherwise the index doesn't work
+        reference_owned.push(b'$');
+
+        let suffix_array = suffix_array(&reference_owned);
+        let burrow_wheeler_transform = bwt(&reference_owned, &suffix_array);
+        let less = less(&burrow_wheeler_transform, &alphabet);
+        let occ = Occ::new(&burrow_wheeler_transform, 1, &alphabet);
+        let fm_index = FMIndex::new(burrow_wheeler_transform, less, occ);
+
+        Self {
+            fm_index,
+            suffix_array,
+        }
+    }
+
+    /// Internal function to perform a backward search on the index
+    fn backward_search(&self, query: &[u8]) -> BackwardSearchResult {
+        self.fm_index.backward_search(query.iter())
+    }
+
+    /// Returns the starting indeces of all full matches of the query in the reference
+    /// Returns an empty vector if no matches are found
+    fn full_matches(&self, query: &[u8]) -> Vec<usize> {
+        let backward_search_result = self.backward_search(query);
+        match backward_search_result {
+            BackwardSearchResult::Complete(suffix_array_interval) => {
+                suffix_array_interval.occ(&self.suffix_array)
+            }
+            _ => Vec::<usize>::new(),
+        }
+    }
+
+    /// Returns matching index if there is exactly one match, otherwise returns None
+    fn single_match(&self, query: &[u8]) -> Option<usize> {
+        let matches = self.full_matches(query);
+        if matches.len() == 1 {
+            Some(matches[0])
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
     // let reference_path = "data/reference.fasta";
     // let query_path = "data/query.fasta";
@@ -23,18 +79,22 @@ fn main() {
     let reference = read_fasta(reference_path).next().unwrap().unwrap();
     let query = read_fasta(query_path);
 
-    let alphabet = alphabets::dna::iupac_alphabet();
+    // Should be abstracted away for our purposes into a class/structure
+    // Allow initialization with one of:
+    // - Path of fastas [take first] (for convenience)
+    // - vec of u8 chars (interfacing with Nextclade)
+    // Save everything internally
+    // fn fm_index.full_matches(subsequence) -> vec[int] (indeces of full matches)
+    // for logging: fn fm_index.best_matches(subsequence) -> IndexSearchResult::{Complete(vec[starting_indeces],Partial(vec[start,len)),Absent}
 
-    let mut ref_seq = reference.seq().to_owned();
-    ref_seq.push(b'$');
-
-    let sa = suffix_array(&ref_seq);
-    let bwt = bwt(&ref_seq, &sa);
-    let less = less(&bwt, &alphabet);
-    let occ = Occ::new(&bwt, 1, &alphabet);
-    let fm = FMIndex::new(&bwt, &less, &occ);
+    // Optional: Abstraction of groups of 3s, same interface, return index of matches spaced by 3 (0::3::-1,1::3::-1,2::3::-1)
+    // Optional: extension around the edges of matches, allow partial mismatches and extend from there - start with accepting only full matches
+    // Get chains of matches, need to optimize some loss function through dynamical programming
+    // Especially tricky: duplications (like in RSV)
 
     let q = 10;
+
+    let index = Index::from_sequence(reference.seq());
 
     for record in query {
         let record = record.expect("Problem parsing sequence");
@@ -44,76 +104,75 @@ fn main() {
         let mut qry_right = 0;
 
         while qstart < record.seq().len().saturating_sub(q as usize) {
-            let qgram = &record.seq()[qstart..qstart + q as usize];
-            let bsr = fm.backward_search(qgram.iter());
-            let matches = match bsr {
-                BackwardSearchResult::Complete(sai) => sai.occ(&sa),
-                BackwardSearchResult::Partial(_, _) => Vec::<usize>::new(),
-                BackwardSearchResult::Absent => Vec::<usize>::new(),
-            };
+            let seed = &record.seq()[qstart..qstart + q as usize];
+            let ref_match = index.single_match(seed);
 
             // extend matches till first mismatch
-            if matches.len() == 1 {
-                // extend backwards
-                let mut ref_left = matches[0];
-                let mut qry_left = qstart;
-                let mut mismatches: u32 = 0;
-                loop {
-                    if mismatches > 1 || qry_left < qry_right || ref_left == 0 || qry_left == 0 {
-                        break;
+            match ref_match {
+                Some(match_index) => {
+                    // extend backwards
+                    let mut ref_left = match_index;
+                    let mut qry_left = qstart;
+                    let mut mismatches: u32 = 0;
+                    loop {
+                        if mismatches > 1 || qry_left < qry_right || ref_left == 0 || qry_left == 0
+                        {
+                            break;
+                        }
+                        if reference.seq()[ref_left - 1] != record.seq()[qry_left - 1] {
+                            mismatches += 1;
+                        }
+                        ref_left -= 1;
+                        qry_left -= 1;
                     }
-                    if reference.seq()[ref_left - 1] != record.seq()[qry_left - 1] {
-                        mismatches += 1;
-                    }
-                    ref_left -= 1;
-                    qry_left -= 1;
-                }
 
-                // extend forwards
-                mismatches = 0;
-                let mut ref_right = matches[0] + q;
-                qry_right = qstart + q;
-                let mut last_mismatch = qry_right;
-                loop {
-                    if mismatches > 1
-                        || ref_right == reference.seq().len()
-                        || qry_right == record.seq().len()
-                    {
-                        break;
+                    // extend forwards
+                    mismatches = 0;
+                    let mut ref_right = match_index + q;
+                    qry_right = qstart + q;
+                    let mut last_mismatch = qry_right;
+                    loop {
+                        if mismatches > 1
+                            || ref_right == reference.seq().len()
+                            || qry_right == record.seq().len()
+                        {
+                            break;
+                        }
+                        if reference.seq()[ref_right] != record.seq()[qry_right] {
+                            mismatches += 1;
+                        }
+                        // Allow 1 mismatch every 50 bases
+                        if qry_right.saturating_sub(last_mismatch) > 50 {
+                            mismatches = mismatches.saturating_sub(1);
+                            last_mismatch = qry_right;
+                        }
+                        ref_right += 1;
+                        qry_right += 1;
                     }
-                    if reference.seq()[ref_right] != record.seq()[qry_right] {
-                        mismatches += 1;
-                    }
-                    // Allow 1 mismatch every 50 bases
-                    if qry_right.saturating_sub(last_mismatch) > 50 {
-                        mismatches = mismatches.saturating_sub(1);
-                        last_mismatch = qry_right;
-                    }
-                    ref_right += 1;
-                    qry_right += 1;
-                }
 
-                if qry_right - qry_left > 25 {
-                    println!(
-                        "diff:{:>5} len:{:>7} ref_index:{:>7} qry_index:{:>7}",
-                        ref_left as i32 - qry_left as i32,
-                        ref_right - ref_left,
-                        ref_left,
-                        qry_left,
-                        // qry_left,
-                        // qry_right,
-                        // ref_left,
-                        // ref_right,
-                        // reference.seq()[ref_left..ref_right]
-                        //     .iter()
-                        //     .map(|&x| x as char)
-                        //     .collect::<String>()
-                    );
-                    qstart = qry_right;
+                    if qry_right - qry_left > 25 {
+                        println!(
+                            "diff:{:>5} len:{:>7} ref_index:{:>7} qry_index:{:>7}",
+                            ref_left as i32 - qry_left as i32,
+                            ref_right - ref_left,
+                            ref_left,
+                            qry_left,
+                            // qry_left,
+                            // qry_right,
+                            // ref_left,
+                            // ref_right,
+                            // reference.seq()[ref_left..ref_right]
+                            //     .iter()
+                            //     .map(|&x| x as char)
+                            //     .collect::<String>()
+                        );
+                        qstart = qry_right;
+                    }
+                    qstart += 1;
                 }
-                qstart += 1;
-            } else {
-                qstart += 20;
+                _ => {
+                    qstart += 20;
+                }
             }
         }
         // Post process matches: join overlapping, remove short ones, remove outliers
