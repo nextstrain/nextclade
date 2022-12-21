c77384efb737b42f982b85b4f0cf0e447170c120
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-12-21 16:30:38 +0100

    Refactor extend into function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ use bio::io::fasta::Records;
 use std::fs::File;
 use std::io::BufReader;
 
+const REFERENCE_PATH: &str = "data/sc2_reference.fasta";
+const QUERY_PATH: &str = "data/sc2_query_long.fasta";
+
+const SEED_MATCH_CONFIG: SeedMatchConfig = SeedMatchConfig {
+    kmer_length: 14,
+    min_match_length: 25,
+    allowed_mismatches: 1,
+};
+
 fn read_fasta(filename: &str) -> Records<BufReader<File>> {
     let path = std::path::Path::new(filename);
     let reader = fasta::Reader::new(File::open(path).unwrap());
@@ -16,7 +25,22 @@ fn read_fasta(filename: &str) -> Records<BufReader<File>> {
 
 struct Index {
     fm_index: FMIndex<BWT, Less, Occ>,
+    ref_seq: Vec<u8>,
     suffix_array: RawSuffixArray,
+}
+
+struct SeedMatchConfig {
+    kmer_length: usize,
+    min_match_length: usize,
+    allowed_mismatches: usize,
+}
+
+#[derive(Clone, Copy)]
+struct SeedMatch {
+    ref_index: usize,
+    qry_index: usize,
+    length: usize,
+    mismatches: usize,
 }
 
 impl Index {
@@ -38,6 +62,7 @@ impl Index {
 
         Self {
             fm_index,
+            ref_seq: reference.to_owned(),
             suffix_array,
         }
     }
@@ -60,10 +85,83 @@ impl Index {
     }
 
     /// Returns matching index if there is exactly one match, otherwise returns None
-    fn single_match(&self, query: &[u8]) -> Option<usize> {
-        let matches = self.full_matches(query);
+    fn single_match(&self, kmer: &[u8]) -> Option<usize> {
+        let matches = self.full_matches(kmer);
         if matches.len() == 1 {
             Some(matches[0])
+        } else {
+            None
+        }
+    }
+
+    /// Given a seed, extends forwards and backwards and returns the extended seed
+    fn extend_seed(
+        &self,
+        qry_seq: &[u8],
+        input_match: &SeedMatch,
+        config: &SeedMatchConfig,
+    ) -> SeedMatch {
+        let SeedMatch {
+            mut ref_index,
+            mut qry_index,
+            mut length,
+            mismatches,
+        } = input_match.clone();
+
+        let mut forward_mismatches = 0;
+
+        while forward_mismatches < config.allowed_mismatches
+            && ref_index + length < self.ref_seq.len()
+            && qry_index + length < qry_seq.len()
+        {
+            if self.ref_seq[ref_index + length] != qry_seq[qry_index + length] {
+                forward_mismatches += 1;
+            }
+
+            length += 1;
+        }
+
+        let mut backward_mismatches = 0;
+
+        while backward_mismatches < config.allowed_mismatches && ref_index > 0 && qry_index > 0 {
+            if self.ref_seq[ref_index - 1] != qry_seq[qry_index - 1] {
+                backward_mismatches += 1;
+            }
+
+            ref_index -= 1;
+            qry_index -= 1;
+            length += 1;
+        }
+
+        SeedMatch {
+            qry_index,
+            ref_index,
+            length,
+            mismatches: mismatches + forward_mismatches + backward_mismatches,
+        }
+    }
+
+    fn single_extended_match(
+        &self,
+        qry_seq: &[u8],
+        qry_index: usize,
+        config: &SeedMatchConfig,
+    ) -> Option<SeedMatch> {
+        let Some(kmer) = &qry_seq.get(qry_index..qry_index + config.kmer_length) else {
+            return None;
+        };
+
+        if let Some(ref_index) = self.single_match(&kmer) {
+            Some(self.extend_seed(
+                qry_seq,
+                &SeedMatch {
+                    qry_index,
+                    ref_index,
+                    length: config.kmer_length,
+                    mismatches: 0,
+                },
+                config,
+            ))
         } else {
             None
         }
@@ -71,20 +169,13 @@ impl Index {
 }
 
 fn main() {
-    // let reference_path = "data/reference.fasta";
-    // let query_path = "data/query.fasta";
-    let reference_path = "data/sc2_reference.fasta";
-    let query_path = "data/sc2_query_long.fasta";
-
-    let reference = read_fasta(reference_path).next().unwrap().unwrap();
-    let query = read_fasta(query_path);
-
     // Should be abstracted away for our purposes into a class/structure
     // Allow initialization with one of:
     // - Path of fastas [take first] (for convenience)
     // - vec of u8 chars (interfacing with Nextclade)
     // Save everything internally
     // fn fm_index.full_matches(subsequence) -> vec[int] (indeces of full matches)
+
     // for logging: fn fm_index.best_matches(subsequence) -> IndexSearchResult::{Complete(vec[starting_indeces],Partial(vec[start,len)),Absent}
 
     // Optional: Abstraction of groups of 3s, same interface, return index of matches spaced by 3 (0::3::-1,1::3::-1,2::3::-1)
@@ -92,7 +183,8 @@ fn main() {
     // Get chains of matches, need to optimize some loss function through dynamical programming
     // Especially tricky: duplications (like in RSV)
 
-    let q = 10;
+    let reference = read_fasta(REFERENCE_PATH).next().unwrap().unwrap();
+    let query = read_fasta(QUERY_PATH);
 
     let index = Index::from_sequence(reference.seq());
 
@@ -100,80 +192,33 @@ fn main() {
         let record = record.expect("Problem parsing sequence");
         println!("Processing sequence: {}", record.id());
 
-        let mut qstart = 0;
-        let mut qry_right = 0;
+        let mut qry_index = 0;
 
-        while qstart < record.seq().len().saturating_sub(q as usize) {
-            let seed = &record.seq()[qstart..qstart + q as usize];
-            let ref_match = index.single_match(seed);
-
+        while qry_index < record.seq().len() {
             // extend matches till first mismatch
-            match ref_match {
-                Some(match_index) => {
-                    // extend backwards
-                    let mut ref_left = match_index;
-                    let mut qry_left = qstart;
-                    let mut mismatches: u32 = 0;
-                    loop {
-                        if mismatches > 1 || qry_left < qry_right || ref_left == 0 || qry_left == 0
-                        {
-                            break;
-                        }
-                        if reference.seq()[ref_left - 1] != record.seq()[qry_left - 1] {
-                            mismatches += 1;
-                        }
-                        ref_left -= 1;
-                        qry_left -= 1;
-                    }
-
-                    // extend forwards
-                    mismatches = 0;
-                    let mut ref_right = match_index + q;
-                    qry_right = qstart + q;
-                    let mut last_mismatch = qry_right;
-                    loop {
-                        if mismatches > 1
-                            || ref_right == reference.seq().len()
-                            || qry_right == record.seq().len()
-                        {
-                            break;
-                        }
-                        if reference.seq()[ref_right] != record.seq()[qry_right] {
-                            mismatches += 1;
-                        }
-                        // Allow 1 mismatch every 50 bases
-                        if qry_right.saturating_sub(last_mismatch) > 50 {
-                            mismatches = mismatches.saturating_sub(1);
-                            last_mismatch = qry_right;
-                        }
-                        ref_right += 1;
-                        qry_right += 1;
-                    }
-
-                    if qry_right - qry_left > 25 {
-                        println!(
-                            "diff:{:>5} len:{:>7} ref_index:{:>7} qry_index:{:>7}",
-                            ref_left as i32 - qry_left as i32,
-                            ref_right - ref_left,
-                            ref_left,
-                            qry_left,
-                            // qry_left,
-                            // qry_right,
-                            // ref_left,
-                            // ref_right,
-                            // reference.seq()[ref_left..ref_right]
-                            //     .iter()
-                            //     .map(|&x| x as char)
-                            //     .collect::<String>()
-                        );
-                        qstart = qry_right;
-                    }
-                    qstart += 1;
-                }
-                _ => {
-                    qstart += 20;
+            if let Some(seed_match) =
+                index.single_extended_match(record.seq(), qry_index, &SEED_MATCH_CONFIG)
+            {
+                if seed_match.length > SEED_MATCH_CONFIG.min_match_length {
+                    println!(
+                        "diff:{:>5} len:{:>7} ref_index:{:>7} qry_index:{:>7}",
+                        seed_match.ref_index as isize - seed_match.qry_index as isize,
+                        seed_match.length,
+                        seed_match.ref_index,
+                        seed_match.qry_index,
+                        // qry_left,
+                        // qry_right,
+                        // ref_left,
+                        // ref_right,
+                        // reference.seq()[ref_left..ref_right]
+                        //     .iter()
+                        //     .map(|&x| x as char)
+                        //     .collect::<String>()
+                    );
+                    qry_index += seed_match.length;
                 }
             }
+            qry_index += 1;
         }
         // Post process matches: join overlapping, remove short ones, remove outliers
         // Design windows based on joined matches, allowing bands, somewhat into the matches where there are gaps
