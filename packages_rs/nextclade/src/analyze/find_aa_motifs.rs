4f2677a16939b3be0401bbbda241580c69a42eaa
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-01-05 00:32:19 +0100

    refactor: split find_aa_motifs function

--- packages_rs/nextclade/src/analyze/find_aa_motifs.rs
+++ packages_rs/nextclade/src/analyze/find_aa_motifs.rs
@@ -16,93 +16,117 @@ pub struct AaMotif {
   pub seq: String,
 }
 
+/// Find motifs in translated sequences, given a list of regexes (with restriction by gene and codon ranges)
+/// This is useful for example to find Fly glycosylation spots.
 pub fn find_aa_motifs(
-  count_aa_motifs_desc: &[AaMotifsDesc],
+  aa_motifs_desc: &[AaMotifsDesc],
   translations: &[Translation],
 ) -> Result<BTreeMap<String, Vec<AaMotif>>, Report> {
-  let motifs: Vec<AaMotif> = count_aa_motifs_desc
+  // Find motifs
+  let motifs: Vec<AaMotif> = aa_motifs_desc
     .iter()
-    .flat_map(
-      |AaMotifsDesc {
-         name,
-         motifs,
-         include_genes,
-         ..
-       }| {
-        // If no genes specified, process all genes
-        let include_genes = if include_genes.is_empty() {
-          translations
-            .iter()
-            .map(|tr| CountAaMotifsGeneDesc {
-              gene: tr.gene_name.clone(),
-              ranges: vec![],
-            })
-            .collect_vec()
-        } else {
-          include_genes.clone()
-        };
-
-        include_genes
-          .iter()
-          .flat_map(|CountAaMotifsGeneDesc { gene, ranges }| {
-            translations
-              .iter()
-              .filter(|Translation { gene_name, .. }| gene_name == gene)
-              .flat_map(|tr| {
-                // If no ranges specified for a gene, search the whole gene
-                let ranges = if ranges.is_empty() {
-                  vec![Range {
-                    begin: 0,
-                    end: tr.seq.len(),
-                  }]
-                } else {
-                  ranges.clone()
-                };
-
-                ranges
-                  .iter()
-                  .flat_map(|range| {
-                    let seq = &tr.seq[range.begin..range.end];
-                    let seq = from_aa_seq(seq);
-
-                    motifs
-                      .iter()
-                      .cloned()
-                      .flat_map(|motif| {
-                        let re = Regex::new(&motif)
-                          .wrap_err_with(|| eyre!("When compiling motif RegEx '{}'", motif))
-                          .unwrap();
-
-                        re.captures_iter(&seq)
-                          .filter_map(|captures| {
-                            captures.get(0).map(|capture| {
-                              Ok(AaMotif {
-                                name: name.clone(),
-                                gene: tr.gene_name.clone(),
-                                position: range.begin + capture.start(),
-                                seq: capture.as_str().to_owned(),
-                              })
-                            })
-                          })
-                          .collect_vec()
-                      })
-                      .collect_vec()
-                  })
-                  .collect_vec()
-              })
-              .collect_vec()
-          })
-          .collect_vec()
-      },
-    )
+    .flat_map(|desc| process_one_aa_motifs_desc(desc, translations))
     .collect::<Result<Vec<AaMotif>, Report>>()?;
 
-  Ok(
-    motifs
-      .into_iter()
-      .group_by(|motif| motif.name.clone())
-      .into_iter()
-      .map(|(name, motifs)| (name, motifs.collect_vec()))
-      .collect(),
-  )
+  // Group motifs by name
+  let motifs = motifs
+    .into_iter()
+    .group_by(|motif| motif.name.clone())
+    .into_iter()
+    .map(|(name, motifs)| (name, motifs.collect_vec()))
+    .collect();
+
+  Ok(motifs)
+}
+
+fn process_one_aa_motifs_desc(
+  aa_motifs_desc: &AaMotifsDesc,
+  translations: &[Translation],
+) -> Vec<Result<AaMotif, Report>> {
+  let AaMotifsDesc {
+    name,
+    motifs,
+    include_genes,
+    ..
+  } = aa_motifs_desc;
+
+  // If no genes specified, process all genes
+  let include_genes = if include_genes.is_empty() {
+    translations
+      .iter()
+      .map(|translation| CountAaMotifsGeneDesc {
+        gene: translation.gene_name.clone(),
+        ranges: vec![],
+      })
+      .collect_vec()
+  } else {
+    include_genes.clone()
+  };
+
+  include_genes
+    .iter()
+    .flat_map(|CountAaMotifsGeneDesc { gene, ranges }| {
+      translations
+        .iter()
+        .filter(|Translation { gene_name, .. }| gene_name == gene)
+        .flat_map(|translation| process_one_translation(translation, name, motifs, ranges))
+        .collect_vec()
+    })
+    .collect_vec()
+}
+
+fn process_one_translation(
+  translation: &Translation,
+  name: &str,
+  motifs: &[String],
+  ranges: &[Range],
+) -> Vec<Result<AaMotif, Report>> {
+  // If no ranges specified for a gene, search the whole gene
+  let ranges = if ranges.is_empty() {
+    vec![Range {
+      begin: 0,
+      end: translation.seq.len(),
+    }]
+  } else {
+    ranges.to_owned()
+  };
+
+  ranges
+    .iter()
+    .flat_map(|range| {
+      let seq = &translation.seq[range.begin..range.end];
+      let seq = from_aa_seq(seq);
+
+      motifs
+        .iter()
+        .cloned()
+        .flat_map(|motif| process_one_motif(name, translation, range, &seq, &motif))
+        .collect_vec()
+    })
+    .collect_vec()
+}
+
+fn process_one_motif(
+  name: &str,
+  translation: &Translation,
+  range: &Range,
+  seq: &str,
+  motif: &str,
+) -> Vec<Result<AaMotif, Report>> {
+  let re = Regex::new(motif)
+    .wrap_err_with(|| eyre!("When compiling motif RegEx '{}'", motif))
+    .unwrap();
+
+  re.captures_iter(seq)
+    .filter_map(|captures| {
+      captures.get(0).map(|capture| {
+        Ok(AaMotif {
+          name: name.to_owned(),
+          gene: translation.gene_name.clone(),
+          position: range.begin + capture.start(),
+          seq: capture.as_str().to_owned(),
+        })
+      })
+    })
+    .collect_vec()
 }
