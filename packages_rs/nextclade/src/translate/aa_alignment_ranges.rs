4fb5a26f477a61b16d3f8a19da54e62201be56dd
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-06-15 17:25:39 +0200

    fix: display of aa unsequenced ranges

--- packages_rs/nextclade/src/translate/aa_alignment_ranges.rs
+++ packages_rs/nextclade/src/translate/aa_alignment_ranges.rs
@@ -1,8 +1,9 @@
 use crate::io::gene_map::GeneMap;
 use crate::translate::coord_map::local_to_codon_range_exclusive;
 use crate::translate::translate_genes::Translation;
-use crate::utils::range::{intersect, NucRefGlobalRange, NucRefLocalRange, PositionLike};
+use crate::utils::range::{intersect, AaRefRange, NucRefGlobalRange, NucRefLocalRange, PositionLike};
 use eyre::Report;
+use itertools::Itertools;
 
 pub fn calculate_aa_alignment_ranges_in_place(
   global_alignment_range: &NucRefGlobalRange,
@@ -34,10 +35,32 @@ pub fn calculate_aa_alignment_ranges_in_place(
     // Record computed AA alignment ranges on CDS translation
     // TODO: avoid mutable code. Move calculation of AA alignment ranges to where translation is.
     //   This requires global_alignment_range to be available there, but it is only computed much later currently.
+    cds_tr.unsequenced_ranges = calculate_aa_unsequenced_ranges(&aa_alignment_ranges, cds_tr.seq.len());
     cds_tr.alignment_ranges = aa_alignment_ranges;
 
     Ok::<(), Report>(())
   })?;
 
   Ok(())
+}
+
+/// Calculate a list of unsequenced AA ranges from a list of AA alignment ranges (which are the sequenced AA ranges)
+fn calculate_aa_unsequenced_ranges(sequenced: &[AaRefRange], cds_tr_len: usize) -> Vec<AaRefRange> {
+  let last_pos = cds_tr_len.into();
+  let sorted_ranges = sequenced.iter().sorted_by_key(|x| x.begin);
+
+  let mut prev_end = 0.into();
+  let mut unsequenced = vec![];
+  for range in sorted_ranges {
+    if range.begin > prev_end {
+      unsequenced.push(AaRefRange::new(prev_end, range.begin));
+    }
+    prev_end = range.end;
+  }
+
+  if last_pos > prev_end {
+    unsequenced.push(AaRefRange::new(prev_end, last_pos));
+  }
+
+  unsequenced
 }
