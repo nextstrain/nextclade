1b8376e5a8fb6ab050ba2aba4265a54c554fc0b1
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-07-06 13:09:18 +0200

    docs: add fixme comment

--- packages_rs/nextclade/src/analyze/divergence.rs
+++ packages_rs/nextclade/src/analyze/divergence.rs
@@ -9,10 +9,15 @@ pub fn calculate_divergence(
   ref_seq_len: usize,
 ) -> f64 {
   // Divergence is just number of substitutions compared to the parent node
+
+  // FIXME: this filtering probably should not be here. Private "substitutions" (the parameter name says
+  //   `private_mutations`, but outside of this function this is called `private_substitutions`) should not contain
+  //   anything that's being filtered out here.
   let mut this_div = private_mutations
     .iter()
     .filter(|m| !m.ref_nuc.is_gap() && m.qry_nuc.is_acgt())
     .count() as f64;
+
   // If divergence is measured per site, divide by the length of reference sequence.
   // The unit of measurement is deduced from what's already is used in the reference tree nodes.
   if &DivergenceUnits::NumSubstitutionsPerYearPerSite == divergence_units {
