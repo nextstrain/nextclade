29770fa6bdc08d81f31140385a5f787e97f4ddf4
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-07-24 16:44:20 +0200

    feat: use another impl of MessagePack for communication between langs

--- packages_rs/nextclade-web/src/io/messagepack.ts
+++ packages_rs/nextclade-web/src/io/messagepack.ts
@@ -1,9 +1,9 @@
-import { unpack, pack } from 'msgpackr'
+import { encode, decode } from '@msgpack/msgpack'
 
 export function messagepackDeserialize<T>(message: Buffer | Uint8Array): T {
-  return unpack(message) as T
+  return decode(message) as T
 }
 
 export function messagepackSerialize<T>(value: T): Uint8Array {
-  return pack(value)
+  return encode(value)
 }
