61505010d25e0bd796c914ec0971fe8c0df5383a
Cornelius Roemer <cornelius.roemer@gmail.com>
2023-08-28 12:54:59 +0200

    Undo N matching penalty for old/new alignment score parity

--- packages_rs/nextclade/src/align/score_matrix.rs
+++ packages_rs/nextclade/src/align/score_matrix.rs
@@ -114,11 +114,7 @@ pub fn score_matrix<T: Letter<T>>(
 
         // ^ If stripes allow to move up diagonally to upper left
         if qpos > stripes[ri - 1].begin && qpos - 1 < stripes[ri - 1].end {
-          score = if qry_seq[qpos - 1].is_unknown() || ref_seq[ri - 1].is_unknown() {
-            // no need to look-up match score since unknown matches with everything.
-            // reduce match score by 1 to de-prioritize matches with unknown states.
-            scores[(ri - 1, qpos - 1)] + params.score_match - 1
-          } else if T::lookup_match_score(qry_seq[qpos - 1], ref_seq[ri - 1]) > 0 {
+          score = if T::lookup_match_score(qry_seq[qpos - 1], ref_seq[ri - 1]) > 0 {
             scores[(ri - 1, qpos - 1)] + params.score_match
           } else {
             scores[(ri - 1, qpos - 1)] - params.penalty_mismatch
