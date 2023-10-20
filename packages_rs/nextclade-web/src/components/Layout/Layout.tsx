2a9e877e71fbcc8906e05f9725ddf707da281610
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-10-20 17:21:22 +0200

    fix(web): tree styles
    Adds small padding for tree filter text, increases z-index of header and footer so that their shadow is always above the tree page svg components.

--- packages_rs/nextclade-web/src/components/Layout/Layout.tsx
+++ packages_rs/nextclade-web/src/components/Layout/Layout.tsx
@@ -16,6 +16,7 @@ const Container = styled.div`
 
 const HeaderWrapper = styled.header`
   height: 45px;
+  z-index: 1001;
 `
 
 const MainInner = styled.main`
@@ -38,7 +39,9 @@ const MainOuter = styled.main`
   margin: 0;
 `
 
-const FooterWrapper = styled.footer``
+const FooterWrapper = styled.footer`
+  z-index: 1001;
+`
 
 export function Layout({ children }: PropsWithChildren<HTMLProps<HTMLDivElement>>) {
   return (
