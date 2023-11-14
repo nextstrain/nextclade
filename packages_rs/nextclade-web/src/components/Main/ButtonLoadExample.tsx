78c1658ea5e7b6f1f8f40b9ff5c9dd6b915d8c08
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-11-14 09:10:32 +0100

    fix(web): run dataset suggest when adding example sequences

--- packages_rs/nextclade-web/src/components/Main/ButtonLoadExample.tsx
+++ packages_rs/nextclade-web/src/components/Main/ButtonLoadExample.tsx
@@ -2,10 +2,13 @@ import React, { useCallback } from 'react'
 import { Button } from 'reactstrap'
 import { useRecoilValue } from 'recoil'
 import { useTranslationSafe } from 'src/helpers/useTranslationSafe'
+import { useRunSeqAutodetect } from 'src/hooks/useRunSeqAutodetect'
+import { useRunAnalysis } from 'src/hooks/useRunAnalysis'
 import { AlgorithmInputDefault } from 'src/io/AlgorithmInput'
 import { datasetCurrentAtom } from 'src/state/dataset.state'
 import { hasInputErrorsAtom } from 'src/state/error.state'
 import { useQuerySeqInputs } from 'src/state/inputs.state'
+import { shouldRunAutomaticallyAtom, shouldSuggestDatasetsOnDatasetPageAtom } from 'src/state/settings.state'
 
 export function ButtonLoadExample({ ...rest }) {
   const { t } = useTranslationSafe()
@@ -13,22 +16,22 @@ export function ButtonLoadExample({ ...rest }) {
   const datasetCurrent = useRecoilValue(datasetCurrentAtom)
   const { addQryInputs } = useQuerySeqInputs()
   const hasInputErrors = useRecoilValue(hasInputErrorsAtom)
-  // const shouldRunAutomatically = useRecoilValue(shouldRunAutomaticallyAtom)
-  // const shouldSuggestDatasets = useRecoilValue(shouldSuggestDatasetsAtom)
-  // const runAnalysis = useRunAnalysis()
-  // const runAutodetect = useRunSeqAutodetect()
+  const shouldRunAutomatically = useRecoilValue(shouldRunAutomaticallyAtom)
+  const shouldSuggestDatasets = useRecoilValue(shouldSuggestDatasetsOnDatasetPageAtom)
+  const runAnalysis = useRunAnalysis()
+  const runAutodetect = useRunSeqAutodetect()
 
   const setExampleSequences = useCallback(() => {
     if (datasetCurrent) {
       addQryInputs([new AlgorithmInputDefault(datasetCurrent)])
-      // if (shouldSuggestDatasets) {
-      //   runAutodetect()
-      // }
-      // if (shouldRunAutomatically) {
-      //   runAnalysis()
-      // }
+      if (shouldSuggestDatasets) {
+        runAutodetect()
+      }
+      if (shouldRunAutomatically) {
+        runAnalysis()
+      }
     }
-  }, [addQryInputs, datasetCurrent])
+  }, [addQryInputs, datasetCurrent, runAnalysis, runAutodetect, shouldRunAutomatically, shouldSuggestDatasets])
 
   return (
     <Button {...rest} color="link" onClick={setExampleSequences} disabled={hasInputErrors || !datasetCurrent}>
