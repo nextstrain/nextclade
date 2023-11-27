433c48a5fcda67cd90175565d15e5f7f2208277f
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-11-27 19:28:39 +0100

    feat(web): add "select dataset" help box

--- packages_rs/nextclade-web/src/components/Main/DatasetSelector.tsx
+++ packages_rs/nextclade-web/src/components/Main/DatasetSelector.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { Container as ContainerBase } from 'reactstrap'
+import { SelectDatasetHelp } from 'src/components/Help/SelectDatasetHelp'
 import { DatasetSelectorList } from 'src/components/Main/DatasetSelectorList'
 import { SuggestionPanel } from 'src/components/Main/SuggestionPanel'
 import { useDatasetSuggestionResults } from 'src/hooks/useRunSeqAutodetect'
@@ -58,35 +59,7 @@ export function DatasetSelectorImpl({
       <Header>
         <Title>
           <H4Inline>{t('Select dataset')}</H4Inline>
-          <InfoButton>
-            <p>
-              {t(
-                'Nextclade software is built to be agnostic to pathogens it analyzes. The information about concrete pathogens is provided in the form of so-called Nextclade datasets.',
-              )}
-            </p>
-            <p>
-              {t(
-                'Datasets vary by the pathogen, strain and other attributes. Each dataset is based on a particular reference sequence. Certain datasets only have enough information for basic analysis, others - more information to allow for more in-depth analysis and checks. Dataset authors periodically update and improve their datasets.',
-              )}
-            </p>
-            <p>
-              {t(
-                'You can select one of the datasets manually or to use automatic dataset suggestion function. Automatic suggestion will attempt to guess the most appropriate dataset from your sequence data.',
-              )}
-            </p>
-            <p>
-              {t(
-                "If you don't find a dataset for a pathogen or a strain you need, then you can create your own dataset. You can also publish it to our community collection, so that other people can use it too.",
-              )}
-            </p>
-            <p>
-              {t('Learn more about Nextclade datasets in the {{documentation}}')}
-              <LinkExternal href="https://docs.nextstrain.org/projects/nextclade/en/stable/user/datasets.html">
-                {t('documentation')}
-              </LinkExternal>
-              {t('.')}
-            </p>
-          </InfoButton>
+          <SelectDatasetHelp />
         </Title>
 
         <SearchBox searchTitle={t('Search datasets')} searchTerm={searchTerm} onSearchTermChange={setSearchTerm} />
@@ -135,6 +108,7 @@ const Main = styled.div`
 `
 
 const Title = styled.h4`
+  display: flex;
   flex: 1;
 `
 
