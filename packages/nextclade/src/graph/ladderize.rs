0b09de74e36a58ba0392926b2cf84f3083ca5b79
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-05-31 18:08:07 +0200

    fix: remove panics from graph ladderize

--- packages/nextclade/src/graph/ladderize.rs
+++ packages/nextclade/src/graph/ladderize.rs
@@ -3,7 +3,7 @@ use crate::graph::graph::Graph;
 use crate::graph::node::{GraphNode, GraphNodeKey};
 use crate::make_internal_report;
 use eyre::{Report, WrapErr};
-use itertools::Itertools;
+use itertools::{izip, Itertools};
 use std::collections::HashMap;
 
 pub fn graph_ladderize<N, E, D>(graph: &mut Graph<N, E, D>) -> Result<(), Report>
@@ -25,13 +25,11 @@ where
   N: GraphNode,
   E: GraphEdge,
 {
-  let node = graph
-    .get_node_mut(node_key)
-    .wrap_err_with(|| make_internal_report!("Node with key {node_key} not found in graph"))?;
+  let node = graph.get_node_mut(node_key).wrap_err("When ladderizing graph")?;
 
   let new_edge_order = new_edge_order_map
     .get(&node_key)
-    .ok_or_else(|| make_internal_report!("Node with key {node_key} not found in edge order map"))?;
+    .ok_or_else(|| make_internal_report!("Node with key {node_key} not found in edge_order_map"))?;
 
   node.outbound_mut().clear();
   for edge_key in new_edge_order {
@@ -39,11 +37,8 @@ where
   }
 
   for edge_key in new_edge_order {
-    ladderize_graph_recursive(
-      graph,
-      graph.get_edge(*edge_key).expect("Edge not found").target(),
-      new_edge_order_map,
-    )?;
+    let edge = graph.get_edge(*edge_key)?.target();
+    ladderize_graph_recursive(graph, edge, new_edge_order_map)?;
   }
 
   Ok(())
@@ -85,11 +80,17 @@ where
   if graph.is_leaf_key(node_key) {
     terminal_count_map.insert(node_key, 1);
   } else {
-    let mut num_terminals = 0;
-    for child in graph.iter_child_keys_of(node) {
-      let child_terminals = terminal_count_map.get(&child).unwrap();
-      num_terminals += child_terminals;
-    }
+    let num_terminals = graph
+      .iter_child_keys_of(node)
+      .map(|child_key| {
+        terminal_count_map
+          .get(&child_key)
+          .ok_or_else(|| make_internal_report!("Node with key {node_key} not found in terminal_count_map"))
+      })
+      .collect::<Result<Vec<_>, Report>>()?
+      .into_iter()
+      .sum();
+
     terminal_count_map.insert(node_key, num_terminals);
   }
 
@@ -108,20 +109,28 @@ where
 {
   let node = graph.get_node(node_key).wrap_err("When preparing ladderize map")?;
 
-  let pre_outbound_order = node.outbound().iter().copied().collect_vec();
+  let pre_outbound_order = node.outbound().iter().collect_vec();
+
   let order_outbound_nodes = pre_outbound_order
     .iter()
-    .map(|edge_key| graph.get_edge(*edge_key).expect("Node not found").target())
-    .map(|node_key| terminal_count_map.get(&node_key).expect("Node not found"))
+    .map(|edge_key| {
+      let node_key = graph
+        .get_edge(**edge_key)
+        .wrap_err_with(|| format!("Node with key {node_key} not found in terminal_count_map"))?
+        .target();
+
+      terminal_count_map
+        .get(&node_key)
+        .ok_or_else(|| make_internal_report!("Node with key {node_key} not found in terminal_count_map"))
+    })
+    .collect::<Result<Vec<_>, Report>>()?;
+
+  let sorted_outbound = izip!(pre_outbound_order.into_iter(), order_outbound_nodes.into_iter())
+    .sorted_by(|&(_, a), &(_, b)| a.cmp(b))
+    .map(|(a, _)| a)
+    .copied()
     .collect_vec();
 
-  let mut zipped = pre_outbound_order
-    .into_iter()
-    .zip(order_outbound_nodes)
-    .collect::<Vec<_>>();
-  zipped.sort_by(|&(_, a), &(_, b)| a.cmp(b));
-
-  let sorted_outbound: Vec<GraphEdgeKey> = zipped.into_iter().map(|(a, _)| a).collect();
   new_edge_order_map.insert(node_key, sorted_outbound);
 
   for child in graph.iter_child_keys_of(node) {
