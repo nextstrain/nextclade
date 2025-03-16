4cfe35b1ae378f3d98056f21c9dc172fb9ca64f8
Richard Neher <richard.neher@unibas.ch>
2025-03-16 19:18:42 +0100

    chore: move test_gff utility script to scripts folder

--- scripts/test_gff.py
+++ scripts/test_gff.py
@@ -2,14 +2,18 @@ from Bio import SeqIO
 from collections import defaultdict
 
 
-# ./target/debug/nextclade run -d community/neherlab/hiv-1/hxb2 --output-all test-hiv hxb2/sequences.fasta
-# run test_gff.py --gff test-hiv/nextclade.gff --fasta hxb2/sequences.fasta
+# ./target/debug/nextclade run -d community/neherlab/hiv-1/hxb2 --output-all test-data/test-hiv hxb2/sequences.fasta
+# run scripts/test_gff.py --gff test-hiv/nextclade.gff --fasta hxb2/sequences.fasta
 
 # ./target/debug/nextclade run -d nextstrain/sars-cov-2/wuhan-hu-1/orfs --output-all test-sc2-orfs data/sars-cov-2/sequences.fasta
-# run test_gff.py --gff test-sc2-orfs/nextclade.gff --fasta data/sars-cov-2/sequences.fasta
+# run scripts/test_gff.py --gff test-sc2-orfs/nextclade.gff --fasta data/sars-cov-2/sequences.fasta
 
 # ./target/debug/nextclade run -d nextstrain/sars-cov-2/wuhan-hu-1/proteins --output-all test-sc2-proteins data/sars-cov-2/sequences.fasta
-# run test_gff.py --gff test-sc2-proteins/nextclade.gff --fasta data/sars-cov-2/sequences.fasta
+# run scripts/test_gff.py --gff test-sc2-proteins/nextclade.gff --fasta data/sars-cov-2/sequences.fasta
+
+# ./target/debug/nextclade run -d hMPXV --output-all test-mpox test_mpox_5p.fasta
+# run scripts/test_gff.py --gff test-mpox/nextclade.gff --fasta test_mpox_5p.fasta
+
 
 if __name__=="__main__":
     # read a fasta file and a gff file from the command line
@@ -36,7 +40,9 @@ if __name__=="__main__":
                 print(f, len(f.sub_features), f.location)
                 if f.type == 'gene':
                     for c in f.sub_features:
-                        print(c.id, c.qualifiers['Name'][0], c.location, c.extract(seq.seq).translate())
+                        phase = int(c.qualifiers.get('phase', [0])[0])
+                        print(c.id, c.qualifiers['Name'][0], c.location, c.translate(seq.seq, start_offset=phase, cds=False))
+                        print("Phase", phase, c.qualifiers.get('truncated-3p', ''), c.qualifiers.get('truncated-5p', ''))
                 # print(f.id, f.qualifiers['Name'][0], Seq.translate(f.extract(seq.seq)))
 
 
