335d5d9e9d4964b8ba62da0e9d886a90379901b3
ivan-aksamentov <ivan.aksamentov@gmail.com>
2025-06-05 07:41:58 +0200

    chore: supress asyncWebAssembly webpack warning
    ```
    The generated code contains 'async/await' because this module is using "asyncWebAssembly".
    However, your target environment does not appear to support 'async/await'.
    As a result, the code may not run as expected or may cause runtime errors.
    ```

--- packages/nextclade-web/config/next/withWasm.ts
+++ packages/nextclade-web/config/next/withWasm.ts
@@ -5,6 +5,7 @@ import { addWebpackConfig } from './lib/addWebpackConfig'
 export default function withWasm(nextConfig: NextConfig) {
   return addWebpackConfig(nextConfig, (nextConfig, webpackConfig) => {
     set(webpackConfig, 'experiments.asyncWebAssembly', true)
+    set(webpackConfig, 'output.environment.asyncFunction', true)
     return webpackConfig
   })
 }
