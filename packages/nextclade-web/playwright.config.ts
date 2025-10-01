82eff0e2ece908692fc48b5e53287cc596ba9437
ivan-aksamentov <ivan.aksamentov@gmail.com>
2025-10-01 10:04:33 +0200

    test: speedup e2e tests

--- packages/nextclade-web/playwright.config.ts
+++ packages/nextclade-web/playwright.config.ts
@@ -6,32 +6,51 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/e2e',
   timeout: 60_000,
-  expect: {
-    timeout: 15_000,
-  },
+  expect: { timeout: 15_000 },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : '50%',
+  retries: process.env.CI ? 1 : 0,
   reporter: [['list'], ['html', { open: 'never' }], ['junit', { outputFile: 'test-results/e2e-results.xml' }]],
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    video: 'off',
     actionTimeout: 10_000,
     navigationTimeout: 30_000,
+    ignoreHTTPSErrors: true,
   },
 
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: [
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-sandbox',
+            '--disable-extensions',
+            '--disable-background-networking',
+            '--disable-default-apps',
+            '--disable-sync',
+            '--metrics-recording-only',
+            '--mute-audio',
+            '--no-first-run',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-renderer-backgrounding',
+          ],
+        },
+      },
     },
   ],
 
   // Global setup and teardown
-  globalSetup: undefined,
+  globalSetup: './tests/e2e/global-setup.ts',
   globalTeardown: undefined,
 
   // Web server (can be used to start the dev server automatically)
