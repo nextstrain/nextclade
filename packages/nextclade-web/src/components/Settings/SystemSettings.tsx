67c6bb44979e1d139463732ec417c62281fd4a12
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-01-08 21:42:41 +0100

    refactor(web): replace formik with useState in SystemSettings

--- packages/nextclade-web/src/components/Settings/SystemSettings.tsx
+++ packages/nextclade-web/src/components/Settings/SystemSettings.tsx
@@ -1,7 +1,6 @@
-import React, { useCallback, useMemo } from 'react'
+import React, { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react'
 import { useRecoilState, useResetRecoilState } from 'recoil'
 import classNames from 'classnames'
-import { Form, Formik, FormikErrors, FormikHelpers, useFormikContext } from 'formik'
 import { debounce } from 'lodash'
 import { MdRefresh } from 'react-icons/md'
 import { Alert, FormGroup, Label } from 'reactstrap'
@@ -15,8 +14,11 @@ import { useTranslationSafe } from 'src/helpers/useTranslationSafe'
 import { NumericInput } from 'src/components/Common/NumericInput'
 import { numThreadsAtom } from 'src/state/settings.state'
 
-export interface SettingsFormValues {
-  numThreads: number
+function validateNumThreads(value: number): string | undefined {
+  if (!Number.isInteger(value) || value < 1 || value > 1000) {
+    return 'Should be a positive integer from 1 to 1000'
+  }
+  return undefined
 }
 
 export function SystemSettings() {
@@ -25,30 +27,38 @@ export function SystemSettings() {
   const [numThreads, setNumThreads] = useRecoilState(numThreadsAtom)
   const resetNumThreads = useResetRecoilState(numThreadsAtom)
   const guess = useGuessNumThreads(numThreads)
-  const handleValidate = useCallback((values: SettingsFormValues): FormikErrors<SettingsFormValues> => {
-    const errors: FormikErrors<SettingsFormValues> = {}
-    const { numThreads } = values
-    if (!Number.isInteger(numThreads) || numThreads < 0 || numThreads > 1000) {
-      errors.numThreads = 'Should be a positive integer from 1 to 1000'
-    }
-    return errors
-  }, [])
+
+  const [localValue, setLocalValue] = useState(numThreads)
+  const [error, setError] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    setLocalValue(numThreads)
+  }, [numThreads])
 
   const setNumThreadsDebounced = useMemo(
-    () => debounce(setNumThreads, 500, { leading: false, trailing: true }), // prettier-ignore
+    () => debounce(setNumThreads, 500, { leading: false, trailing: true }),
     [setNumThreads],
   )
 
-  const handleSubmit = useCallback(
-    (values: SettingsFormValues, { setSubmitting }: FormikHelpers<SettingsFormValues>) => {
-      setNumThreadsDebounced(values.numThreads)
-      setSubmitting(false)
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const value = Number.parseInt(e.target.value, 10)
+      setLocalValue(value)
+
+      const validationError = validateNumThreads(value)
+      setError(validationError)
+
+      if (!validationError) {
+        setNumThreadsDebounced(value)
+      }
     },
     [setNumThreadsDebounced],
   )
 
-  const initialValues = useMemo(() => ({ numThreads }), [numThreads])
-  const onReset = useCallback(() => ({ numThreads }), [numThreads])
+  const handleReset = useCallback(() => {
+    resetNumThreads()
+    setError(undefined)
+  }, [resetNumThreads])
 
   const memoryAvailable = useMemo(() => {
     return guess.memoryAvailable ? prettyBytes.format(guess.memoryAvailable) : t('unsupported')
@@ -59,93 +69,67 @@ export function SystemSettings() {
   }, [guess.memoryAvailable, numThreads, t])
 
   return (
-    <Formik initialValues={initialValues} validate={handleValidate} onSubmit={handleSubmit} onReset={onReset}>
-      {({ values, errors, touched, handleChange, handleBlur, resetForm }) => (
-        <Form>
-          <FormikAutoSubmit />
+    <form>
+      <FormGroup>
+        <Label className="d-block w-100">
+          <NumericInput
+            id="numThreads"
+            min={1}
+            max={1000}
+            className={classNames('d-inline', error && 'border-danger')}
+            type="number"
+            identifier="settings-num-threads-input"
+            value={localValue}
+            onChange={handleChange}
+          />
+          <span className="d-inline">
+            <span className="mx-3">{t('Number of CPU threads')}</span>
+            <span className="mx-auto">
+              <ButtonTransparent className="my-0" type="button" title={t('Reset to default')} onClick={handleReset}>
+                <MdRefresh /> {t('Reset')}
+              </ButtonTransparent>
+            </span>
+          </span>
+          {error && <p className="text-danger">{error}</p>}
+          {guess.numThreads && guess.memoryAvailable && (
+            <Alert className="mt-2 p-1" color="primary" isOpen fade={false}>
+              <TableSlim borderless className="small mb-1">
+                <tbody>
+                  <tr>
+                    <td>{t('Memory available*')}</td>
+                    <td>{memoryAvailable}</td>
+                  </tr>
 
-          <FormGroup>
-            <Label className="d-block w-100">
-              <NumericInput
-                id="numThreads"
-                min={1}
-                max={1000}
-                className={classNames('d-inline', errors?.numThreads && 'border-danger')}
-                type="number"
-                identifier="settings-num-threads-input"
-                value={values.numThreads}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              <span className="d-inline">
-                <span className="mx-3">{t('Number of CPU threads')}</span>
-                <span className="mx-auto">
-                  <ButtonTransparent
-                    className="my-0"
-                    type="button"
-                    title={t('Reset to default')}
-                    // eslint-disable-next-line react-perf/jsx-no-new-function-as-prop
-                    onClick={() => {
-                      resetNumThreads()
-                      resetForm()
-                    }}
-                  >
-                    <MdRefresh /> {t('Reset')}
-                  </ButtonTransparent>
-                </span>
-              </span>
-              {touched.numThreads && errors?.numThreads && <p className="text-danger">{errors.numThreads}</p>}
-              {guess.numThreads && guess.memoryAvailable && (
-                <Alert className="mt-2 p-1" color="primary" isOpen fade={false}>
-                  <TableSlim borderless className="small mb-1">
-                    <tbody>
-                      <tr>
-                        <td>{t('Memory available*')}</td>
-                        <td>{memoryAvailable}</td>
-                      </tr>
+                  <tr>
+                    <td>{t('Memory per CPU thread')}</td>
+                    <td>{memoryAvailablePerThread}</td>
+                  </tr>
 
-                      <tr>
-                        <td>{t('Memory per CPU thread')}</td>
-                        <td>{memoryAvailablePerThread}</td>
-                      </tr>
+                  <tr>
+                    <td>{t('Recommended number of CPU threads**')}</td>
+                    <td>{guess.numThreads ?? t('unsupported')}</td>
+                  </tr>
 
-                      <tr>
-                        <td>{t('Recommended number of CPU threads**')}</td>
-                        <td>{guess.numThreads ?? t('unsupported')}</td>
-                      </tr>
+                  <tr>
+                    <td colSpan={2} className="small">
+                      {t('* Current value. This amount can change depending on load')}
+                    </td>
+                  </tr>
 
-                      <tr>
-                        <td colSpan={2} className="small">
-                          {t('* Current value. This amount can change depending on load')}
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td colSpan={2} className="small">
-                          {t('** {{appName}} requires at least {{memoryRequired}} of memory per thread', {
-                            appName: PROJECT_NAME,
-                            memoryRequired: prettyBytes.format(MEMORY_BYTES_PER_THREAD_MINIMUM),
-                          })}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </TableSlim>
-                </Alert>
-              )}
-            </Label>
-          </FormGroup>
-        </Form>
-      )}
-    </Formik>
+                  <tr>
+                    <td colSpan={2} className="small">
+                      {t('** {{appName}} requires at least {{memoryRequired}} of memory per thread', {
+                        appName: PROJECT_NAME,
+                        memoryRequired: prettyBytes.format(MEMORY_BYTES_PER_THREAD_MINIMUM),
+                      })}
+                    </td>
+                  </tr>
+                </tbody>
+              </TableSlim>
+            </Alert>
+          )}
+        </Label>
+      </FormGroup>
+    </form>
   )
-}
-
-export function FormikAutoSubmit() {
-  const { values, submitForm, isValid } = useFormikContext()
-  React.useEffect(() => {
-    if (isValid) {
-      void submitForm() // eslint-disable-line no-void
-    }
-  }, [values, submitForm, isValid])
-  return null
 }
